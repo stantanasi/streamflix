054b29dc1f87f6ebfe5bf58283439cf017079f91
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-21 22:48:17 +0200

    feat(home-view-model): use LiveData

--- app/src/main/java/com/tanasi/sflix/fragments/home/HomeViewModel.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/home/HomeViewModel.kt
@@ -1,6 +1,21 @@
 package com.tanasi.sflix.fragments.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.tanasi.sflix.models.Movie
+import com.tanasi.sflix.models.TvShow
 
 class HomeViewModel : ViewModel() {
+
+    private val _state: MutableLiveData<State> = MutableLiveData(State.Loading)
+    val state: LiveData<State> = _state
+
+    sealed class State {
+        object Loading : State()
+        data class SuccessLoading(
+            val trendingMovies: List<Movie>,
+            val trendingTvShows: List<TvShow>
+        ) : State()
+    }
 }
