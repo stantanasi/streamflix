cd32eb5ca0a842f1ffa92b78057042aba1f3cdd7
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-22 00:32:15 +0200

    feat: create MoviePresenter

---
+++ app/src/main/java/com/tanasi/sflix/presenters/MoviePresenter.kt
@@ -0,0 +1,19 @@
+package com.tanasi.sflix.presenters
+
+import android.view.ViewGroup
+import androidx.leanback.widget.Presenter
+
+class MoviePresenter : Presenter() {
+
+    override fun onCreateViewHolder(parent: ViewGroup?): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder?, item: Any?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUnbindViewHolder(viewHolder: ViewHolder?) {
+        TODO("Not yet implemented")
+    }
+}
