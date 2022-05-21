609ad24a09e6bfcb56b88509dd31d960399e8717
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-22 01:13:17 +0200

    feat: display home data

--- app/src/main/java/com/tanasi/sflix/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/home/HomeFragment.kt
@@ -4,11 +4,22 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.leanback.app.BrowseSupportFragment
+import androidx.leanback.widget.ArrayObjectAdapter
+import androidx.leanback.widget.HeaderItem
+import androidx.leanback.widget.ListRow
+import androidx.leanback.widget.ListRowPresenter
 import com.tanasi.sflix.R
+import com.tanasi.sflix.models.Movie
+import com.tanasi.sflix.models.TvShow
+import com.tanasi.sflix.presenters.MoviePresenter
+import com.tanasi.sflix.presenters.TvShowPresenter
 
 class HomeFragment : BrowseSupportFragment() {
 
     private val viewModel by viewModels<HomeViewModel>()
+
+    private val trendingMovies = mutableListOf<Movie>()
+    private val trendingTvShows = mutableListOf<TvShow>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -19,10 +30,42 @@ class HomeFragment : BrowseSupportFragment() {
             when (state) {
                 HomeViewModel.State.Loading -> {}
                 is HomeViewModel.State.SuccessLoading -> {
+                    trendingMovies.apply {
+                        clear()
+                        addAll(state.trendingMovies)
+                    }
+                    trendingTvShows.apply {
+                        clear()
+                        addAll(state.trendingTvShows)
+                    }
+                    displayHome()
                 }
             }
         }
 
         viewModel.fetchHome()
+    }
+
+
+    private fun displayHome() {
+        val rowsAdapter = ArrayObjectAdapter(ListRowPresenter())
+
+        rowsAdapter.add(ListRow(
+            HeaderItem("Trending Movies"),
+            ArrayObjectAdapter(MoviePresenter()).apply {
+                clear()
+                addAll(0, trendingMovies)
+            }
+        ))
+
+        rowsAdapter.add(ListRow(
+            HeaderItem("Trending TV Shows"),
+            ArrayObjectAdapter(TvShowPresenter()).apply {
+                clear()
+                addAll(0, trendingTvShows)
+            }
+        ))
+
+        adapter = rowsAdapter
     }
 }
