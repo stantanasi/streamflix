16af2b2019d216c73fb8863286118df2ba571ad4
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-21 22:52:34 +0200

    feat(home-fragment): initialize ViewModel

--- app/src/main/java/com/tanasi/sflix/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/home/HomeFragment.kt
@@ -1,10 +1,13 @@
 package com.tanasi.sflix.fragments.home
 
 import android.os.Bundle
+import androidx.fragment.app.viewModels
 import androidx.leanback.app.BrowseSupportFragment
 import com.tanasi.sflix.R
 
 class HomeFragment : BrowseSupportFragment() {
+
+    private val viewModel by viewModels<HomeViewModel>()
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
