0d4746277fd497d36bcaf92da2438516b3c8d3c5
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-21 22:49:16 +0200

    feat(home-view-model): build SflixService

--- app/src/main/java/com/tanasi/sflix/fragments/home/HomeViewModel.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/home/HomeViewModel.kt
@@ -5,8 +5,11 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.tanasi.sflix.models.Movie
 import com.tanasi.sflix.models.TvShow
+import com.tanasi.sflix.services.SflixService
 
 class HomeViewModel : ViewModel() {
+
+    private val sflixService = SflixService.build()
 
     private val _state: MutableLiveData<State> = MutableLiveData(State.Loading)
     val state: LiveData<State> = _state
