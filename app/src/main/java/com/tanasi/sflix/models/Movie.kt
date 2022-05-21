e02a0a5fb480fe5b1635772c81c6ae674e2f7672
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-21 22:40:11 +0200

    feat(movie.model): add movie's properties

--- app/src/main/java/com/tanasi/sflix/models/Movie.kt
+++ app/src/main/java/com/tanasi/sflix/models/Movie.kt
@@ -1,5 +1,12 @@
 package com.tanasi.sflix.models
 
 class Movie(
+    val id: String,
+    val title: String,
+    val year: Int,
+    val quality: String,
+    val rating: Double,
+    val poster: String,
+    val banner: String? = null,
 ) {
 }
