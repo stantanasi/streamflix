28af4338a5e417c94f93532a6aff0d628c4ada8c
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-22 00:40:55 +0200

    feat: create TvShowPresenter

---
+++ app/src/main/java/com/tanasi/sflix/presenters/TvShowPresenter.kt
@@ -0,0 +1,19 @@
+package com.tanasi.sflix.presenters
+
+import android.view.ViewGroup
+import androidx.leanback.widget.Presenter
+
+class TvShowPresenter : Presenter() {
+
+    override fun onCreateViewHolder(parent: ViewGroup?): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder?, item: Any?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUnbindViewHolder(viewHolder: ViewHolder?) {
+        TODO("Not yet implemented")
+    }
+}
