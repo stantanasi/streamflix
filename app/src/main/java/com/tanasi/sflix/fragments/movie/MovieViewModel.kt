7e33374c841ff89dce5fe45f2374ed19456879ba
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-22 12:36:09 +0200

    feat(MovieViewModel): implement LiveData

--- app/src/main/java/com/tanasi/sflix/fragments/movie/MovieViewModel.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/movie/MovieViewModel.kt
@@ -1,6 +1,20 @@
 package com.tanasi.sflix.fragments.movie
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.tanasi.sflix.models.Movie
+import com.tanasi.sflix.services.SflixService
 
 class MovieViewModel : ViewModel() {
+
+    private val sflixService = SflixService.build()
+
+    private val _state: MutableLiveData<State> = MutableLiveData(State.Loading)
+    val state: LiveData<State> = _state
+
+    sealed class State {
+        object Loading : State()
+        data class SuccessLoading(val movie: Movie) : State()
+    }
 }
