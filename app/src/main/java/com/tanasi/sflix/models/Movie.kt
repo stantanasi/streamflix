529de305f768baafebc958f9910e8e1aec51976a
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-22 12:39:59 +0200

    feat(Movie): parse movie's quality to enum

--- app/src/main/java/com/tanasi/sflix/models/Movie.kt
+++ app/src/main/java/com/tanasi/sflix/models/Movie.kt
@@ -4,9 +4,25 @@ class Movie(
     val id: String,
     val title: String,
     val year: Int,
-    val quality: String,
+    quality: String,
     val rating: Double,
     val poster: String,
     val banner: String? = null,
 ) {
+
+    val quality = Quality.getByValue(quality)
+
+
+    enum class Quality {
+        HD,
+        CAM;
+
+        companion object {
+            fun getByValue(value: String): Quality? = try {
+                valueOf(value)
+            } catch (e: Exception) {
+                null
+            }
+        }
+    }
 }
