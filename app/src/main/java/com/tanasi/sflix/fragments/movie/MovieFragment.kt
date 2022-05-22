06c321552ee1e8e1217519a3bb937630df5824d3
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-22 23:02:20 +0200

    feat(MovieFragment): display movie's servers

--- app/src/main/java/com/tanasi/sflix/fragments/movie/MovieFragment.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/movie/MovieFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
@@ -60,6 +61,14 @@ class MovieFragment : Fragment() {
         binding.tvMovieTitle.text = movie.title
 
         binding.tvMovieQuality.text = movie.quality?.name ?: "N/A"
+
+        binding.llMovieServers.apply {
+            movie.servers.forEach {
+                val button = Button(requireContext())
+                button.text = it.name
+                addView(button)
+            }
+        }
 
         binding.tvMovieOverview.text = movie.overview
 
