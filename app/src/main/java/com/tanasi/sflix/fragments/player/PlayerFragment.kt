17bf20dc30cbd8a6136eb8bbbc21ea4e0bf941f2
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-23 11:33:02 +0200

    feat(PlayerFragment): initialize SafeArgs and ViewModel

--- app/src/main/java/com/tanasi/sflix/fragments/player/PlayerFragment.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/player/PlayerFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.tanasi.sflix.databinding.FragmentPlayerBinding
 
 
@@ -12,6 +14,9 @@ class PlayerFragment : Fragment() {
 
     private var _binding: FragmentPlayerBinding? = null
     private val binding get() = _binding!!
+
+    private val args by navArgs<PlayerFragmentArgs>()
+    private val viewModel by viewModels<PlayerViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
