d8cbc662d76d190869d0bc5d08000f8297a09707
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-23 11:21:59 +0200

    feat(SflixService): add requests to get video source url
    Add getLink request method
    Add getSources request method

--- app/src/main/java/com/tanasi/sflix/services/SflixService.kt
+++ app/src/main/java/com/tanasi/sflix/services/SflixService.kt
@@ -29,4 +29,47 @@ interface SflixService {
 
     @GET("ajax/movie/episodes/{id}")
     suspend fun fetchMovieServers(@Path("id") movieId: String): Document
+
+
+    @GET("ajax/get_link/{id}")
+    suspend fun getLink(@Path("id") id: String): Link
+
+    @GET
+    @Headers(
+        "accept: */*",
+        "referer: https://sflix.to",
+        "x-requested-with: XMLHttpRequest",
+    )
+    suspend fun getSources(
+        @Url url: String,
+        @Query("id") id: String,
+    ): Sources
+
+
+    data class Link(
+        val type: String = "",
+        val link: String = "",
+        val sources: List<String> = listOf(),
+        val tracks: List<String> = listOf(),
+        val title: String = "",
+    )
+
+    data class Sources(
+        val sources: List<Source> = listOf(),
+        val sourcesBackup: List<Source> = listOf(),
+        val tracks: List<Track> = listOf(),
+        val server: Int? = null,
+    ) {
+        data class Source(
+            val file: String = "",
+            val type: String = "",
+        )
+
+        data class Track(
+            val file: String = "",
+            val label: String = "",
+            val kind: String = "",
+            val default: Boolean = false,
+        )
+    }
 }
