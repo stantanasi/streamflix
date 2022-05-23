7510a0dc87d66011c90c4fc8c6aadd2f12ef67f3
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-23 11:33:59 +0200

    feat(PlayerFragment): display VideoSupportFragment

--- app/src/main/java/com/tanasi/sflix/fragments/player/PlayerFragment.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/player/PlayerFragment.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.leanback.app.VideoSupportFragment
 import androidx.navigation.fragment.navArgs
 import com.tanasi.sflix.databinding.FragmentPlayerBinding
+import com.tanasi.sflix.models.Video
 
 
 class PlayerFragment : Fragment() {
@@ -18,6 +20,9 @@ class PlayerFragment : Fragment() {
     private val args by navArgs<PlayerFragmentArgs>()
     private val viewModel by viewModels<PlayerViewModel>()
 
+    private val videoFragment = VideoSupportFragment()
+    private lateinit var video: Video
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,5 +30,14 @@ class PlayerFragment : Fragment() {
     ): View {
         _binding = FragmentPlayerBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        parentFragmentManager
+            .beginTransaction()
+            .replace(binding.flPlayer.id, videoFragment)
+            .commit()
     }
 }
