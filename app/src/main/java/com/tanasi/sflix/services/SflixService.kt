14a4f3c5897cb5610d734db7743eb0172073cd53
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-23 10:15:19 +0200

    feat(SflixService): add GsonConverter to Retrofit.Builder

--- app/src/main/java/com/tanasi/sflix/services/SflixService.kt
+++ app/src/main/java/com/tanasi/sflix/services/SflixService.kt
@@ -3,8 +3,8 @@ package com.tanasi.sflix.services
 import com.tanasi.retrofit_jsoup.converter.JsoupConverterFactory
 import org.jsoup.nodes.Document
 import retrofit2.Retrofit
-import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.*
 
 interface SflixService {
 
@@ -13,6 +13,7 @@ interface SflixService {
             val retrofit = Retrofit.Builder()
                 .baseUrl("https://sflix.to/")
                 .addConverterFactory(JsoupConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
                 .build()
 
             return retrofit.create(SflixService::class.java)
