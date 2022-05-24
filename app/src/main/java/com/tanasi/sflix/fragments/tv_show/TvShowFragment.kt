ebf2d1676c812cea4298eb12fcb56688391e11d3
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-24 17:18:58 +0200

    feat(TvShowFragment): observe TvShowViewModel state LiveData

--- app/src/main/java/com/tanasi/sflix/fragments/tv_show/TvShowFragment.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/tv_show/TvShowFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.tanasi.sflix.databinding.FragmentTvShowBinding
 
 class TvShowFragment : Fragment() {
 
     private var _binding: FragmentTvShowBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel by viewModels<TvShowViewModel>()
+    private val args by navArgs<TvShowFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,5 +24,17 @@ class TvShowFragment : Fragment() {
     ): View {
         _binding = FragmentTvShowBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.state.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                TvShowViewModel.State.Loading -> {}
+                is TvShowViewModel.State.SuccessLoading -> {
+                }
+            }
+        }
     }
 }
