b2a66c3dfbbde54fda32e178308d55baf61cc9e2
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-25 22:58:24 +0200

    feat(PlayerFragment): implement ExoPlayer's player

--- app/src/main/java/com/tanasi/sflix/fragments/player/PlayerFragment.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/player/PlayerFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 import com.tanasi.sflix.databinding.FragmentPlayerBinding
 import com.tanasi.sflix.models.Video
 
@@ -19,6 +21,7 @@ class PlayerFragment : Fragment() {
     private val args by navArgs<PlayerFragmentArgs>()
     private val viewModel by viewModels<PlayerViewModel>()
 
+    private lateinit var player: ExoPlayer
     private lateinit var video: Video
 
     override fun onCreateView(
@@ -33,6 +36,9 @@ class PlayerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        player = ExoPlayer.Builder(requireContext()).build()
+        binding.pvPlayer.player = player
+
         viewModel.state.observe(viewLifecycleOwner) { state ->
             when (state) {
                 PlayerViewModel.State.Loading -> {}
@@ -46,7 +52,16 @@ class PlayerFragment : Fragment() {
         viewModel.getVideo(args.linkId)
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        player.release()
+    }
+
 
     private fun displayVideo() {
+        player.setMediaItem(MediaItem.fromUri(video.source))
+
+        player.prepare()
+        player.play()
     }
 }
