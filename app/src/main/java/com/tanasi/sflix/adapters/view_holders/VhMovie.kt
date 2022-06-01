87f4bf8384884e7422d39964d066d8a94a27c157
Lory-Stan TANASI <stantanasi@gmail.com>
2022-06-01 20:53:52 +0200

    feat(VhMovie): implement on movie clicked

--- app/src/main/java/com/tanasi/sflix/adapters/view_holders/VhMovie.kt
+++ app/src/main/java/com/tanasi/sflix/adapters/view_holders/VhMovie.kt
@@ -1,11 +1,13 @@
 package com.tanasi.sflix.adapters.view_holders
 
 import android.view.animation.AnimationUtils
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 import com.bumptech.glide.Glide
 import com.tanasi.sflix.R
 import com.tanasi.sflix.databinding.ItemMovieBinding
+import com.tanasi.sflix.fragments.home.HomeFragmentDirections
 import com.tanasi.sflix.models.Movie
 
 class VhMovie(
@@ -27,13 +29,20 @@ class VhMovie(
 
 
     private fun displayCard(binding: ItemMovieBinding) {
-        binding.root.setOnFocusChangeListener { _, hasFocus ->
-            val animation = when {
-                hasFocus -> AnimationUtils.loadAnimation(context, R.anim.zoom_in)
-                else -> AnimationUtils.loadAnimation(context, R.anim.zoom_out)
+        binding.root.apply {
+            setOnClickListener {
+                findNavController().navigate(
+                    HomeFragmentDirections.actionHomeToMovie(movie.id)
+                )
             }
-            binding.root.startAnimation(animation)
-            animation.fillAfter = true
+            setOnFocusChangeListener { _, hasFocus ->
+                val animation = when {
+                    hasFocus -> AnimationUtils.loadAnimation(context, R.anim.zoom_in)
+                    else -> AnimationUtils.loadAnimation(context, R.anim.zoom_out)
+                }
+                binding.root.startAnimation(animation)
+                animation.fillAfter = true
+            }
         }
 
         Glide.with(context)
