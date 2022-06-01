6fe998f557303811e4940b30b005c84d47727409
Lory-Stan TANASI <stantanasi@gmail.com>
2022-06-01 22:28:23 +0200

    feat(VhMovie): display movie header

--- app/src/main/java/com/tanasi/sflix/adapters/view_holders/VhMovie.kt
+++ app/src/main/java/com/tanasi/sflix/adapters/view_holders/VhMovie.kt
@@ -1,14 +1,18 @@
 package com.tanasi.sflix.adapters.view_holders
 
 import android.view.animation.AnimationUtils
+import android.widget.Button
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 import com.bumptech.glide.Glide
 import com.tanasi.sflix.R
 import com.tanasi.sflix.databinding.ItemMovieBinding
+import com.tanasi.sflix.databinding.ItemMovieHeaderBinding
 import com.tanasi.sflix.fragments.home.HomeFragmentDirections
+import com.tanasi.sflix.fragments.movie.MovieFragmentDirections
 import com.tanasi.sflix.models.Movie
+import com.tanasi.sflix.utils.format
 
 class VhMovie(
     private val _binding: ViewBinding
@@ -24,6 +28,8 @@ class VhMovie(
 
         when (_binding) {
             is ItemMovieBinding -> displayCard(_binding)
+
+            is ItemMovieHeaderBinding -> displayHeader(_binding)
         }
     }
 
@@ -51,5 +57,38 @@ class VhMovie(
             .into(binding.ivMoviePoster)
 
         binding.tvMovieTitle.text = movie.title
+    }
+
+
+    private fun displayHeader(binding: ItemMovieHeaderBinding) {
+        Glide.with(context)
+            .load(movie.poster)
+            .into(binding.ivMoviePoster)
+
+        binding.tvMovieTitle.text = movie.title
+
+        binding.tvMovieQuality.text = movie.quality?.name ?: "N/A"
+
+        binding.llMovieServers.apply {
+            movie.servers.forEach {
+                val button = Button(context)
+                button.text = it.name
+                button.setOnClickListener { _ ->
+                    findNavController().navigate(
+                        MovieFragmentDirections.actionMovieToPlayer(
+                            linkId = it.id,
+                            title = movie.title
+                        )
+                    )
+                }
+                addView(button)
+            }
+        }
+
+        binding.tvMovieOverview.text = movie.overview
+
+        binding.tvMovieReleased.text = movie.released?.format("yyyy-MM-dd")
+
+        binding.tvMovieRuntime.text = "${movie.runtime} min"
     }
 }
