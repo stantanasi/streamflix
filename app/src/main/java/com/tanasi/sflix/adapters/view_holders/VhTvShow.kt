eed153aaafa922f117499253fec992cd2e089eb9
Lory-Stan TANASI <stantanasi@gmail.com>
2022-06-01 20:40:46 +0200

    feat(VhTvShow): display TV show card view

--- app/src/main/java/com/tanasi/sflix/adapters/view_holders/VhTvShow.kt
+++ app/src/main/java/com/tanasi/sflix/adapters/view_holders/VhTvShow.kt
@@ -1,7 +1,11 @@
 package com.tanasi.sflix.adapters.view_holders
 
+import android.view.animation.AnimationUtils
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
+import com.bumptech.glide.Glide
+import com.tanasi.sflix.R
+import com.tanasi.sflix.databinding.ItemTvShowBinding
 import com.tanasi.sflix.models.TvShow
 
 class VhTvShow(
@@ -15,5 +19,28 @@ class VhTvShow(
 
     fun bind(tvShow: TvShow) {
         this.tvShow = tvShow
+
+        when (_binding) {
+            is ItemTvShowBinding -> displayCard(_binding)
+        }
+    }
+
+
+    private fun displayCard(binding: ItemTvShowBinding) {
+        binding.root.setOnFocusChangeListener { _, hasFocus ->
+            val animation = when {
+                hasFocus -> AnimationUtils.loadAnimation(context, R.anim.zoom_in)
+                else -> AnimationUtils.loadAnimation(context, R.anim.zoom_out)
+            }
+            binding.root.startAnimation(animation)
+            animation.fillAfter = true
+        }
+
+        Glide.with(context)
+            .load(tvShow.poster)
+            .centerCrop()
+            .into(binding.ivTvShowPoster)
+
+        binding.tvTvShowTitle.text = tvShow.title
     }
 }
