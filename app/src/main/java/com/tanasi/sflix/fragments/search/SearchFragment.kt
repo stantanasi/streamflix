639a61befe43dc28f65526a9b471343c4b69e301
Lory-Stan TANASI <stantanasi@gmail.com>
2022-06-02 17:30:54 +0200

    feat: create SearchFragment

---
+++ app/src/main/java/com/tanasi/sflix/fragments/search/SearchFragment.kt
@@ -0,0 +1,30 @@
+package com.tanasi.sflix.fragments.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.tanasi.sflix.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel by viewModels<SearchViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/tanasi/sflix/fragments/search/SearchViewModel.kt
@@ -0,0 +1,18 @@
+package com.tanasi.sflix.fragments.search
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.tanasi.sflix.services.SflixService
+
+class SearchViewModel : ViewModel() {
+
+    private val sflixService = SflixService.build()
+
+    private val _state: MutableLiveData<State> = MutableLiveData(State.Loading)
+    val state: LiveData<State> = _state
+
+    sealed class State {
+        object Loading : State()
+    }
+}
