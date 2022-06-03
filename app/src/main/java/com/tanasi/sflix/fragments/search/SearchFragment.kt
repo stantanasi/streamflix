f0195425c2b6b159c9652ad6934f54e65f4b1016
Lory-Stan TANASI <stantanasi@gmail.com>
2022-06-03 20:47:31 +0200

    feat(SearchFragment): search movies, TV shows on EditText text changed

--- app/src/main/java/com/tanasi/sflix/fragments/search/SearchFragment.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/search/SearchFragment.kt
@@ -1,12 +1,15 @@
 package com.tanasi.sflix.fragments.search
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.tanasi.sflix.databinding.FragmentSearchBinding
+import java.util.*
 
 class SearchFragment : Fragment() {
 
@@ -26,5 +29,23 @@ class SearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.etSearch.addTextChangedListener(object : TextWatcher {
+            private var timer = Timer()
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable) {
+                val query = s.toString().trim()
+                timer.cancel()
+                timer = Timer()
+                timer.schedule(object : TimerTask() {
+                    override fun run() {
+                        viewModel.search(query)
+                    }
+                }, 1000)
+            }
+        })
     }
 }
