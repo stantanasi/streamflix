8b64f8988051ede7a22966f369c70e62e9108601
Lory-Stan TANASI <stantanasi@gmail.com>
2022-06-08 15:15:29 +0200

    fix(search): query empty string

--- app/src/main/java/com/tanasi/sflix/fragments/search/SearchViewModel.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/search/SearchViewModel.kt
@@ -29,68 +29,72 @@ class SearchViewModel : ViewModel() {
     fun search(query: String) = viewModelScope.launch {
         _state.value = State.Searching
 
-        val document = sflixService.search(query.replace(" ", "-"))
+        _state.value = if (query.isEmpty()) {
+            State.SuccessSearching(listOf())
+        } else {
+            val document = sflixService.search(query.replace(" ", "-"))
 
-        _state.value = State.SuccessSearching(
-            document.select("div.flw-item").map {
+            State.SuccessSearching(
+                document.select("div.flw-item").map {
 
-                val isMovie = it.selectFirst("a")?.attr("href")?.contains("/movie/") ?: false
+                    val isMovie = it.selectFirst("a")?.attr("href")?.contains("/movie/") ?: false
 
-                val info = it
-                    .select("div.film-detail > div.fd-infor > span")
-                    .toList()
-                    .map { element -> element.text() }
-                    .takeIf { info -> info.size == 3 }
+                    val info = it
+                        .select("div.film-detail > div.fd-infor > span")
+                        .toList()
+                        .map { element -> element.text() }
+                        .takeIf { info -> info.size == 3 }
 
-                val id = it.selectFirst("a")?.attr("href")?.substringAfterLast("-") ?: ""
-                val title = it.select("h2.film-name").text()
-                val released = info?.get(2) ?: ""
-                val quality = info?.get(1) ?: ""
-                val rating = info?.get(0)?.toDoubleOrNull()
-                val poster = it.selectFirst("div.film-poster > img.film-poster-img").let { img ->
-                    img?.attr("data-src") ?: img?.attr("src")
-                } ?: ""
+                    val id = it.selectFirst("a")?.attr("href")?.substringAfterLast("-") ?: ""
+                    val title = it.select("h2.film-name").text()
+                    val released = info?.get(2) ?: ""
+                    val quality = info?.get(1) ?: ""
+                    val rating = info?.get(0)?.toDoubleOrNull()
+                    val poster = it.selectFirst("div.film-poster > img.film-poster-img").let { img ->
+                        img?.attr("data-src") ?: img?.attr("src")
+                    } ?: ""
 
-                when (isMovie) {
-                    true -> Movie(
-                        id = id,
-                        title = title,
-                        released = released,
-                        quality = quality,
-                        rating = rating,
-                        poster = poster,
-                    )
-                    false -> TvShow(
-                        id = id,
-                        title = title,
-                        quality = quality,
-                        rating = rating,
-                        poster = poster,
+                    when (isMovie) {
+                        true -> Movie(
+                            id = id,
+                            title = title,
+                            released = released,
+                            quality = quality,
+                            rating = rating,
+                            poster = poster,
+                        )
+                        false -> TvShow(
+                            id = id,
+                            title = title,
+                            quality = quality,
+                            rating = rating,
+                            poster = poster,
 
-                        seasons = info?.get(2)?.let { lastEpisode ->
-                            listOf(
-                                Season(
-                                    id = "",
-                                    number = lastEpisode
-                                        .substringAfter("S")
-                                        .substringBefore(":")
-                                        .toInt(),
+                            seasons = info?.get(2)?.let { lastEpisode ->
+                                listOf(
+                                    Season(
+                                        id = "",
+                                        number = lastEpisode
+                                            .substringAfter("S")
+                                            .substringBefore(":")
+                                            .toInt(),
 
-                                    episodes = listOf(
-                                        Episode(
-                                            id = "",
-                                            number = lastEpisode
-                                                .substringAfter(":")
-                                                .substringAfter("E")
-                                                .toInt()
+                                        episodes = listOf(
+                                            Episode(
+                                                id = "",
+                                                number = lastEpisode
+                                                    .substringAfter(":")
+                                                    .substringAfter("E")
+                                                    .toInt()
+                                            )
                                         )
                                     )
                                 )
-                            )
-                        } ?: listOf(),
-                    )
+                            } ?: listOf(),
+                        )
+                    }
                 }
-            }
-        )
+            )
+        }
     }
 }
