9a433ede2807347a8f39f0fd5335d1912f4de95d
Lory-Stan TANASI <stantanasi@gmail.com>
2022-06-14 10:23:49 +0200

    feat(navigation): create NavigationSlideView

---
+++ navigation/src/main/java/com/tanasi/navigation/widget/NavigationSlideView.kt
@@ -0,0 +1,14 @@
+package com.tanasi.navigation.widget
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+@SuppressLint("RestrictedApi")
+class NavigationSlideView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0,
+) : FrameLayout(context, attrs, defStyle) {
+}
