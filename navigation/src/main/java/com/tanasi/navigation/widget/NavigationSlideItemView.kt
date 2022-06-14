6233bbd6f77acf9cac77930de972b9f58d89487d
Lory-Stan TANASI <stantanasi@gmail.com>
2022-06-14 15:05:25 +0200

    feat(NavigationSlideItemView): implement members

--- navigation/src/main/java/com/tanasi/navigation/widget/NavigationSlideItemView.kt
+++ navigation/src/main/java/com/tanasi/navigation/widget/NavigationSlideItemView.kt
@@ -3,12 +3,18 @@ package com.tanasi.navigation.widget
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.drawable.Drawable
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.view.menu.MenuItemImpl
 import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.appcompat.widget.TooltipCompat
+import androidx.core.view.PointerIconCompat
+import androidx.core.view.ViewCompat
 import com.tanasi.navigation.R
 
 @SuppressLint("RestrictedApi")
@@ -25,42 +31,88 @@ class NavigationSlideItemView(
     private val icon = view.findViewById<ImageView>(R.id.iv_navigation_item_icon)
     private val label = view.findViewById<TextView>(R.id.tv_navigation_item_label)
 
+    var itemPosition = INVALID_ITEM_POSITION
 
-    override fun initialize(itemData: MenuItemImpl?, menuType: Int) {
-        TODO("Not yet implemented")
+    private lateinit var itemData: MenuItemImpl
+
+
+    override fun initialize(itemData: MenuItemImpl, menuType: Int) {
+        this.itemData = itemData
+        setCheckable(itemData.isCheckable)
+        setChecked(itemData.isChecked)
+        isEnabled = itemData.isEnabled
+        setIcon(itemData.icon)
+        setTitle(itemData.title)
+        id = itemData.itemId
+        if (!TextUtils.isEmpty(itemData.contentDescription)) {
+            contentDescription = itemData.contentDescription
+        }
+
+        val tooltipText = when {
+            itemData.tooltipText?.isNotEmpty() == true -> itemData.tooltipText
+            else -> itemData.title
+        }
+
+        // Avoid calling tooltip for L and M devices because long pressing twice may freeze devices.
+        if (VERSION.SDK_INT > VERSION_CODES.M) {
+            TooltipCompat.setTooltipText(this, tooltipText)
+        }
+        visibility = if (itemData.isVisible) VISIBLE else GONE
     }
 
-    override fun getItemData(): MenuItemImpl {
-        TODO("Not yet implemented")
-    }
+    override fun getItemData(): MenuItemImpl = itemData
 
     override fun setTitle(title: CharSequence?) {
-        TODO("Not yet implemented")
+        label.text = title
     }
 
     override fun setCheckable(checkable: Boolean) {
-        TODO("Not yet implemented")
+        refreshDrawableState()
     }
 
     override fun setChecked(checked: Boolean) {
-        TODO("Not yet implemented")
+        refreshDrawableState()
+
+        // Set the item as selected to send an AccessibilityEvent.TYPE_VIEW_SELECTED from View, so that
+        // the item is read out as selected.
+        isSelected = checked
+    }
+
+    override fun setEnabled(enabled: Boolean) {
+        super.setEnabled(enabled)
+        label.isEnabled = enabled
+        icon.isEnabled = enabled
+        if (enabled) {
+            ViewCompat.setPointerIcon(
+                this, PointerIconCompat.getSystemIcon(context, PointerIconCompat.TYPE_HAND)
+            )
+        } else {
+            ViewCompat.setPointerIcon(this, null)
+        }
+    }
+
+    override fun onCreateDrawableState(extraSpace: Int): IntArray {
+        val drawableState = super.onCreateDrawableState(extraSpace + 1)
+        if (itemData.isCheckable && itemData.isChecked) {
+            mergeDrawableStates(drawableState, CHECKED_STATE_SET)
+        }
+        return drawableState
     }
 
     override fun setShortcut(showShortcut: Boolean, shortcutKey: Char) {
-        TODO("Not yet implemented")
     }
 
-    override fun setIcon(icon: Drawable?) {
-        TODO("Not yet implemented")
+    override fun setIcon(drawable: Drawable?) {
+        if (drawable === icon.drawable) {
+            return
+        }
+
+        icon.setImageDrawable(drawable)
     }
 
-    override fun prefersCondensedTitle(): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun prefersCondensedTitle(): Boolean = false
 
-    override fun showsIcon(): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun showsIcon(): Boolean = true
 
 
     /**
@@ -68,4 +120,10 @@ class NavigationSlideItemView(
      * this menu item view.
      */
     fun getItemLayoutResId(): Int = R.layout.item_navigation
+
+
+    companion object {
+        private const val INVALID_ITEM_POSITION = -1
+        private val CHECKED_STATE_SET = intArrayOf(android.R.attr.state_checked)
+    }
 }
