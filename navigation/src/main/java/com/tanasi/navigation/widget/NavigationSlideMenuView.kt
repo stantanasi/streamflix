6d9f379121de059d41bebd91c549deba3f79f35c
Lory-Stan TANASI <stantanasi@gmail.com>
2022-06-14 10:24:46 +0200

    feat(navigation): create NavigationSlideMenuView

---
+++ navigation/src/main/java/com/tanasi/navigation/widget/NavigationSlideMenuView.kt
@@ -0,0 +1,21 @@
+package com.tanasi.navigation.widget
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.widget.LinearLayout
+import androidx.appcompat.view.menu.MenuBuilder
+import androidx.appcompat.view.menu.MenuView
+
+@SuppressLint("RestrictedApi")
+class NavigationSlideMenuView(
+    context: Context,
+) : LinearLayout(context), MenuView {
+
+    override fun initialize(menu: MenuBuilder?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getWindowAnimations(): Int {
+        TODO("Not yet implemented")
+    }
+}
