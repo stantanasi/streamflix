d503a268a1e5155b0073b0dc9d8cdca5cdde27b2
Lory-Stan TANASI <stantanasi@gmail.com>
2022-06-14 11:06:24 +0200

    feat(navigation): create NavigationSlideMenu

---
+++ navigation/src/main/java/com/tanasi/navigation/widget/NavigationSlideMenu.kt
@@ -0,0 +1,11 @@
+package com.tanasi.navigation.widget
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.appcompat.view.menu.MenuBuilder
+
+@SuppressLint("RestrictedApi")
+class NavigationSlideMenu(
+    context: Context,
+) : MenuBuilder(context) {
+}
