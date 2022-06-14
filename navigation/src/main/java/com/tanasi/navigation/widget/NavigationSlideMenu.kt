3c951701cf341487a216f46d2ea5e6855482c858
Lory-Stan TANASI <stantanasi@gmail.com>
2022-06-14 15:41:33 +0200

    feat(NavigationSlideMenu): set menu item checkable

--- navigation/src/main/java/com/tanasi/navigation/widget/NavigationSlideMenu.kt
+++ navigation/src/main/java/com/tanasi/navigation/widget/NavigationSlideMenu.kt
@@ -2,10 +2,25 @@ package com.tanasi.navigation.widget
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.view.MenuItem
+import android.view.SubMenu
 import androidx.appcompat.view.menu.MenuBuilder
+import androidx.appcompat.view.menu.MenuItemImpl
 
 @SuppressLint("RestrictedApi")
 class NavigationSlideMenu(
     context: Context,
 ) : MenuBuilder(context) {
+
+    override fun addInternal(
+        group: Int, id: Int, categoryOrder: Int, title: CharSequence
+    ): MenuItem {
+        stopDispatchingItemsChanged()
+        val item = super.addInternal(group, id, categoryOrder, title)
+        if (item is MenuItemImpl) {
+            item.isExclusiveCheckable = true
+        }
+        startDispatchingItemsChanged()
+        return item
+    }
 }
