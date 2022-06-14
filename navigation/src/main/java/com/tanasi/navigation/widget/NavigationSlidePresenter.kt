bedd232c6e707f7f269bbe5c209820ef2f18a911
Lory-Stan TANASI <stantanasi@gmail.com>
2022-06-14 11:07:13 +0200

    feat(navigation): create NavigationSlidePresenter

---
+++ navigation/src/main/java/com/tanasi/navigation/widget/NavigationSlidePresenter.kt
@@ -0,0 +1,59 @@
+package com.tanasi.navigation.widget
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Parcelable
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.*
+
+@SuppressLint("RestrictedApi")
+class NavigationSlidePresenter : MenuPresenter {
+
+    override fun initForMenu(context: Context?, menu: MenuBuilder?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getMenuView(root: ViewGroup?): MenuView {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateMenuView(cleared: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setCallback(cb: MenuPresenter.Callback?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSubMenuSelected(subMenu: SubMenuBuilder?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCloseMenu(menu: MenuBuilder?, allMenusAreClosing: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun flagActionItems(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun expandItemActionView(menu: MenuBuilder?, item: MenuItemImpl?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun collapseItemActionView(menu: MenuBuilder?, item: MenuItemImpl?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun getId(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSaveInstanceState(): Parcelable {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        TODO("Not yet implemented")
+    }
+}
