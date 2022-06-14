5a635a575e16f594575202a9c2e1d5ad36c75b39
Lory-Stan TANASI <stantanasi@gmail.com>
2022-06-14 14:40:26 +0200

    feat(navigation-item-view): inflate menu item layout

--- navigation/src/main/java/com/tanasi/navigation/widget/NavigationSlideItemView.kt
+++ navigation/src/main/java/com/tanasi/navigation/widget/NavigationSlideItemView.kt
@@ -3,14 +3,28 @@ package com.tanasi.navigation.widget
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
 import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.view.menu.MenuItemImpl
 import androidx.appcompat.view.menu.MenuView.ItemView
+import com.tanasi.navigation.R
 
 @SuppressLint("RestrictedApi")
 class NavigationSlideItemView(
     context: Context,
 ) : FrameLayout(context), ItemView {
+
+    private val view = LayoutInflater.from(context).inflate(
+        getItemLayoutResId(),
+        this,
+        true
+    )
+
+    private val icon = view.findViewById<ImageView>(R.id.iv_navigation_item_icon)
+    private val label = view.findViewById<TextView>(R.id.tv_navigation_item_label)
+
 
     override fun initialize(itemData: MenuItemImpl?, menuType: Int) {
         TODO("Not yet implemented")
@@ -47,4 +61,11 @@ class NavigationSlideItemView(
     override fun showsIcon(): Boolean {
         TODO("Not yet implemented")
     }
+
+
+    /**
+     * Returns the unique identifier to the layout resource that must be used to render the items in
+     * this menu item view.
+     */
+    fun getItemLayoutResId(): Int = R.layout.item_navigation
 }
