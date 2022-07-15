52d1c006fcd97d11df904cd191aeae7925913a9a
Lory-Stan TANASI <stantanasi@gmail.com>
2022-07-15 14:50:52 +0200

    feat(seasons): implement fetch TV Show's seasons

--- app/src/main/java/com/tanasi/sflix/fragments/seasons/SeasonsViewModel.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/seasons/SeasonsViewModel.kt
@@ -3,9 +3,11 @@ package com.tanasi.sflix.fragments.seasons
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.tanasi.sflix.models.Episode
 import com.tanasi.sflix.models.Season
 import com.tanasi.sflix.services.SflixService
+import kotlinx.coroutines.launch
 
 class SeasonsViewModel : ViewModel() {
 
@@ -24,7 +26,28 @@ class SeasonsViewModel : ViewModel() {
             val seasonId: String,
             val episodes: List<Episode>,
         ) : State()
-
         data class FailedLoadingEpisodes(val error: Exception) : State()
+    }
+
+
+    fun getSeasons(tvShowId: String) = viewModelScope.launch {
+        _state.value = State.LoadingSeasons
+
+        _state.value = try {
+            State.SuccessLoadingSeasons(
+                seasons = sflixService.fetchTvShowSeasons(tvShowId)
+                    .select("div.dropdown-menu.dropdown-menu-model > a")
+                    .mapIndexed { seasonNumber, seasonElement ->
+                        val seasonId = seasonElement.attr("data-id")
+                        Season(
+                            id = seasonId,
+                            number = seasonNumber + 1,
+                            title = seasonElement.text(),
+                        )
+                    }
+            )
+        } catch (e: Exception) {
+            State.FailedLoadingSeasons(e)
+        }
     }
 }
