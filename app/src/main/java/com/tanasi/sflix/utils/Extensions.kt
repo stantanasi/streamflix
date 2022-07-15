3eee4b13991caeaec3c643d769e78b34f8809e3c
Lory-Stan TANASI <stantanasi@gmail.com>
2022-07-15 17:33:28 +0200

    feat: create extension to get current fragment

--- app/src/main/java/com/tanasi/sflix/utils/Extensions.kt
+++ app/src/main/java/com/tanasi/sflix/utils/Extensions.kt
@@ -5,6 +5,10 @@ import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.fragment.NavHostFragment
+import com.tanasi.sflix.R
+import com.tanasi.sflix.activities.MainActivity
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -42,4 +46,15 @@ fun Activity.hideKeyboard() {
 fun Context.hideKeyboard(view: View) {
     val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+fun Context.toActivity(): FragmentActivity? = this as? FragmentActivity
+
+fun FragmentActivity.getCurrentFragment(): Fragment? = when (this) {
+    is MainActivity -> {
+        val navHostFragment =
+            this.supportFragmentManager.findFragmentById(R.id.nav_main_fragment) as NavHostFragment
+        navHostFragment.childFragmentManager.fragments.firstOrNull()
+    }
+    else -> null
 }
