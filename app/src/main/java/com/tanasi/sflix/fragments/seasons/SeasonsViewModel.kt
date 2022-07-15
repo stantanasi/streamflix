2813e71b63f64cd4f641302872ffa4250c72a4f1
Lory-Stan TANASI <stantanasi@gmail.com>
2022-07-15 14:52:07 +0200

    feat(seasons): implement fetch season episodes

--- app/src/main/java/com/tanasi/sflix/fragments/seasons/SeasonsViewModel.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/seasons/SeasonsViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.tanasi.sflix.models.Episode
 import com.tanasi.sflix.models.Season
+import com.tanasi.sflix.models.Server
 import com.tanasi.sflix.services.SflixService
 import kotlinx.coroutines.launch
 
@@ -48,6 +49,49 @@ class SeasonsViewModel : ViewModel() {
             )
         } catch (e: Exception) {
             State.FailedLoadingSeasons(e)
+        }
+    }
+
+    fun getSeasonEpisodes(seasonId: String) = viewModelScope.launch {
+        _state.value = State.LoadingEpisodes
+
+        _state.value = try {
+            State.SuccessLoadingEpisodes(
+                seasonId = seasonId,
+                episodes = sflixService.fetchSeasonEpisode(seasonId)
+                    .select("div.flw-item.film_single-item.episode-item.eps-item")
+                    .mapIndexed { episodeNumber, episodeElement ->
+                        val episodeId = episodeElement.attr("data-id")
+                        Episode(
+                            id = episodeId,
+                            number = episodeElement
+                                .selectFirst("div.episode-number")
+                                ?.text()
+                                ?.substringAfter("Episode ")
+                                ?.substringBefore(":")
+                                ?.toIntOrNull()
+                                ?: episodeNumber,
+                            title = episodeElement
+                                .selectFirst("h3.film-name")
+                                ?.text()
+                                ?: "",
+                            poster = episodeElement.selectFirst("img")
+                                ?.attr("src") ?: "",
+
+                            servers = sflixService.fetchEpisodeServers(episodeId)
+                                .select("a")
+                                .map {
+                                    Server(
+                                        id = it.attr("data-id"),
+                                        name = it.selectFirst("span")?.text()
+                                            ?.trim() ?: "",
+                                    )
+                                },
+                        )
+                    }
+            )
+        } catch (e: Exception) {
+            State.FailedLoadingEpisodes(e)
         }
     }
 }
