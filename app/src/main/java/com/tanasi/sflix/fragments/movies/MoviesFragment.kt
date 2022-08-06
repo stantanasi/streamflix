344817419c336cd99ee10572b48793e58865493e
Lory-Stan TANASI <stantanasi@gmail.com>
2022-08-06 13:08:42 +0200

    feat(movies): display movies list

--- app/src/main/java/com/tanasi/sflix/fragments/movies/MoviesFragment.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/movies/MoviesFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.tanasi.sflix.R
+import com.tanasi.sflix.adapters.SflixAdapter
 import com.tanasi.sflix.databinding.FragmentMoviesBinding
+import com.tanasi.sflix.models.Movie
 
 class MoviesFragment : Fragment() {
 
@@ -15,16 +19,52 @@ class MoviesFragment : Fragment() {
 
     private val viewModel by viewModels<MoviesViewModel>()
 
+    private val movies = mutableListOf<Movie>()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentMoviesBinding.inflate(inflater, container, false)
+        if (_binding == null) {
+            _binding = FragmentMoviesBinding.inflate(inflater, container, false)
+            viewModel.getMovies()
+        }
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel.state.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                MoviesViewModel.State.Loading -> {}
+
+                is MoviesViewModel.State.SuccessLoading -> {
+                    movies.let {
+                        it.clear()
+                        it.addAll(state.movies)
+                    }
+                    displayMovies()
+                }
+                is MoviesViewModel.State.FailedLoading -> {
+                    Toast.makeText(
+                        requireContext(),
+                        state.error.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
+
+
+    private fun displayMovies() {
+        binding.vgvMovies.apply {
+            adapter = SflixAdapter(movies.onEach {
+                it.itemType = SflixAdapter.Type.MOVIE_MOVIES
+            })
+            setItemSpacing(requireContext().resources.getDimension(R.dimen.movies_spacing).toInt())
+        }
     }
 }
