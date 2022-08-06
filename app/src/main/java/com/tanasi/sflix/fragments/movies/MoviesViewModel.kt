c2ab8ba8c1221beaf8cdd0650c9d1d545337b00f
Lory-Stan TANASI <stantanasi@gmail.com>
2022-08-06 12:59:59 +0200

    feat(movies): implement get movies

--- app/src/main/java/com/tanasi/sflix/fragments/movies/MoviesViewModel.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/movies/MoviesViewModel.kt
@@ -3,8 +3,10 @@ package com.tanasi.sflix.fragments.movies
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.tanasi.sflix.models.Movie
 import com.tanasi.sflix.services.SflixService
+import kotlinx.coroutines.launch
 
 class MoviesViewModel : ViewModel() {
 
@@ -18,5 +20,57 @@ class MoviesViewModel : ViewModel() {
 
         data class SuccessLoading(val movies: List<Movie>) : State()
         data class FailedLoading(val error: Exception) : State()
+    }
+
+    fun getMovies() = viewModelScope.launch {
+        _state.value = State.Loading
+
+        _state.value = try {
+            val document = sflixService.fetchMovies()
+
+            val movies = document
+                .select("div.flw-item")
+                .map {
+                    val info = it
+                        .select("div.film-detail > div.fd-infor > span")
+                        .toList()
+                        .map { element -> element.text() }
+                        .let { info ->
+                            object {
+                                val released = when (info.size) {
+                                    1 -> info[0] ?: ""
+                                    2 -> info[1] ?: ""
+                                    3 -> info[2] ?: ""
+                                    else -> null
+                                }
+                                val quality = when (info.size) {
+                                    3 -> info[1] ?: ""
+                                    else -> null
+                                }
+                                val rating = when (info.size) {
+                                    2 -> info[0].toDoubleOrNull()
+                                    3 -> info[0].toDoubleOrNull()
+                                    else -> null
+                                }
+                            }
+                        }
+
+                    Movie(
+                        id = it.selectFirst("a")?.attr("href")?.substringAfterLast("-") ?: "",
+                        title = it.select("h2.film-name").text(),
+                        released = info.released ?: "",
+                        quality = info.quality ?: "",
+                        rating = info.rating,
+                        poster = it.selectFirst("div.film-poster > img.film-poster-img")
+                            .let { img ->
+                                img?.attr("data-src") ?: img?.attr("src")
+                            } ?: "",
+                    )
+                }
+
+            State.SuccessLoading(movies)
+        } catch (e: Exception) {
+            State.FailedLoading(e)
+        }
     }
 }
