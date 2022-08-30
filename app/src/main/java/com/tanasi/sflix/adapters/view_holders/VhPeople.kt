8334c24e58cd8b8734f3cd624f8aa2b9223c7f68
Lory-Stan TANASI <stantanasi@gmail.com>
2022-08-30 22:52:54 +0200

    feat(VhPeople): implement displaying people

--- app/src/main/java/com/tanasi/sflix/adapters/view_holders/VhPeople.kt
+++ app/src/main/java/com/tanasi/sflix/adapters/view_holders/VhPeople.kt
@@ -1,7 +1,11 @@
 package com.tanasi.sflix.adapters.view_holders
 
+import android.view.animation.AnimationUtils
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
+import com.bumptech.glide.Glide
+import com.tanasi.sflix.R
+import com.tanasi.sflix.databinding.ItemPeopleBinding
 import com.tanasi.sflix.models.People
 
 class VhPeople(
@@ -17,6 +21,34 @@ class VhPeople(
         this.people = people
 
         when (_binding) {
+            is ItemPeopleBinding -> displayPeople(_binding)
         }
+    }
+
+
+    private fun displayPeople(binding: ItemPeopleBinding) {
+        binding.root.apply {
+            setOnClickListener {
+            }
+            setOnFocusChangeListener { _, hasFocus ->
+                val animation = when {
+                    hasFocus -> AnimationUtils.loadAnimation(context, R.anim.zoom_in)
+                    else -> AnimationUtils.loadAnimation(context, R.anim.zoom_out)
+                }
+                binding.root.startAnimation(animation)
+                animation.fillAfter = true
+            }
+        }
+
+        binding.ivPeopleImage.apply {
+            clipToOutline = true
+            Glide.with(context)
+                .load(people.image)
+                .placeholder(R.drawable.ic_person_placeholder)
+                .centerCrop()
+                .into(this)
+        }
+
+        binding.tvPeopleName.text = people.name
     }
 }
