6589eee4d19b79b6c456d3fb5041fea97eb6239f
Lory-Stan TANASI <stantanasi@gmail.com>
2022-09-04 22:38:22 +0200

    feat(season): remove TV show seasons list

--- app/src/main/java/com/tanasi/sflix/fragments/season/SeasonFragment.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/season/SeasonFragment.kt
@@ -8,7 +8,6 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
-import com.bumptech.glide.Glide
 import com.tanasi.sflix.adapters.SflixAdapter
 import com.tanasi.sflix.databinding.FragmentSeasonBinding
 
@@ -32,33 +31,8 @@ class SeasonFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Glide.with(requireContext())
-            .load(args.tvShowBanner)
-            .centerCrop()
-            .into(binding.ivTvShowBanner)
-
-        binding.tvTvShowTitle.text = args.tvShowTitle
-
         viewModel.state.observe(viewLifecycleOwner) { state ->
             when (state) {
-                SeasonViewModel.State.LoadingSeasons -> {}
-                is SeasonViewModel.State.SuccessLoadingSeasons -> {
-                    binding.vgvSeasons.apply {
-                        adapter = SflixAdapter(state.seasons.onEach {
-                            it.itemType = SflixAdapter.Type.SEASON
-                        })
-                        setItemSpacing(20)
-                    }
-                    viewModel.getSeasonEpisodes(state.seasons.firstOrNull()?.id ?: "")
-                }
-                is SeasonViewModel.State.FailedLoadingSeasons -> {
-                    Toast.makeText(
-                        requireContext(),
-                        state.error.message,
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-
                 SeasonViewModel.State.LoadingEpisodes -> {}
                 is SeasonViewModel.State.SuccessLoadingEpisodes -> {
                     binding.vgvEpisodes.apply {
@@ -77,7 +51,5 @@ class SeasonFragment : Fragment() {
                 }
             }
         }
-
-        viewModel.getSeasons(args.tvShowId)
     }
 }
