aac7c29962dc61fb188fe8a39a788f47c484aef2
Lory-Stan TANASI <stantanasi@gmail.com>
2022-09-04 22:44:01 +0200

    feat(tv-show): implement on season clicked event

--- app/src/main/java/com/tanasi/sflix/adapters/view_holders/VhSeason.kt
+++ app/src/main/java/com/tanasi/sflix/adapters/view_holders/VhSeason.kt
@@ -1,10 +1,12 @@
 package com.tanasi.sflix.adapters.view_holders
 
 import android.view.animation.AnimationUtils
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 import com.tanasi.sflix.R
 import com.tanasi.sflix.databinding.ItemSeasonBinding
+import com.tanasi.sflix.fragments.tv_show.TvShowFragmentDirections
 import com.tanasi.sflix.models.Season
 
 
@@ -28,6 +30,13 @@ class VhSeason(
 
     private fun displaySeason(binding: ItemSeasonBinding) {
         binding.root.apply {
+            setOnClickListener {
+                findNavController().navigate(
+                    TvShowFragmentDirections.actionTvShowToSeason(
+                        seasonId = season.id,
+                    )
+                )
+            }
             setOnFocusChangeListener { _, hasFocus ->
                 val animation = when {
                     hasFocus -> AnimationUtils.loadAnimation(context, R.anim.zoom_in)
