8198812893d461004060672439ac10b2a0ddf4ad
Lory-Stan TANASI <stantanasi@gmail.com>
2022-09-06 15:38:16 +0200

    feat(people): add movies/tvShows properties

--- app/src/main/java/com/tanasi/sflix/models/People.kt
+++ app/src/main/java/com/tanasi/sflix/models/People.kt
@@ -6,6 +6,9 @@ class People(
     val slug: String,
     val name: String,
     val image: String = "",
+
+    val movies: List<Movie> = listOf(),
+    val tvShows: List<TvShow> = listOf(),
 ) : SflixAdapter.Item {
 
     override var itemType = SflixAdapter.Type.PEOPLE
