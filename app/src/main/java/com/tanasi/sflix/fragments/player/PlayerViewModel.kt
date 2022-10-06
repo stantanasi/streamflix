ad71139930bcb4aefa9999afb93c17c2d5aca728
Lory-Stan TANASI <stantanasi@gmail.com>
2022-10-06 15:37:11 +0200

    fix(player): retrieve source url
    Use sflixService.getSources to get source url, instead of WebSocket

--- app/src/main/java/com/tanasi/sflix/fragments/player/PlayerViewModel.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/player/PlayerViewModel.kt
@@ -4,14 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.gson.Gson
 import com.tanasi.sflix.models.Video
 import com.tanasi.sflix.services.SflixService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import okhttp3.*
-import org.json.JSONArray
-import org.json.JSONObject
 
 class PlayerViewModel : ViewModel() {
 
@@ -47,68 +43,35 @@ class PlayerViewModel : ViewModel() {
 
             val link = sflixService.getLink(servers.firstOrNull()?.id ?: "")
 
-            OkHttpClient.Builder().build().newWebSocket(
-                Request.Builder()
-                    .url("wss://wsx.dokicloud.one/socket.io/?EIO=4&transport=websocket")
-                    .build(),
-                object : WebSocketListener() {
-                    private lateinit var key: String
-
-                    override fun onMessage(webSocket: WebSocket, text: String) {
-                        val response = Regex("^(\\d*)(.*)").find(text).let {
-                            object {
-                                val code = it?.groupValues?.getOrNull(1)?.toIntOrNull() ?: 0
-                                val data = it?.groupValues?.getOrNull(2) ?: ""
-                            }
-                        }
-
-                        when (response.code) {
-                            0 -> webSocket.send("40")
-                            40 -> {
-                                key = JSONObject(response.data).optString("sid")
-                                val linkId =
-                                    link.link.substringAfterLast("/").substringBefore("?")
-                                webSocket.send("""42["getSources",{"id":"$linkId"}]""")
-                            }
-                            42 -> {
-                                val json = JSONArray(response.data)
-
-                                val sources = Gson().fromJson(
-                                    json.opt(1).toString(),
-                                    SflixService.Sources.Encrypted::class.java,
-                                ).decrypt(
-                                    secret = key
-                                )
-
-                                val video = Video(
-                                    source = sources.sources.firstOrNull()?.file ?: "",
-                                    subtitles = sources.tracks
-                                        .filter { it.kind == "captions" }
-                                        .map {
-                                            Video.Subtitle(
-                                                label = it.label,
-                                                file = it.file,
-                                                default = it.default,
-                                            )
-                                        }
-                                )
-
-                                _state.postValue(State.SuccessLoading(video))
-
-                                webSocket.send("41")
-                            }
-                        }
-                    }
-
-                    override fun onFailure(
-                        webSocket: WebSocket,
-                        t: Throwable,
-                        response: Response?
-                    ) {
-                        _state.postValue(State.FailedLoading(Exception(t)))
-                    }
-                }
+            val response = sflixService.getSources(
+                url = link.link
+                    .substringBeforeLast("/")
+                    .replace("/embed", "/ajax/embed")
+                    .plus("/getSources"),
+                id = link.link.substringAfterLast("/").substringBefore("?"),
             )
+
+            val sources = when (response) {
+                is SflixService.Sources -> response
+                is SflixService.Sources.Encrypted -> response.decrypt(
+                    secret = sflixService.getSourceEncryptedKey().text()
+                )
+            }
+
+            val video = Video(
+                source = sources.sources.firstOrNull()?.file ?: "",
+                subtitles = sources.tracks
+                    .filter { it.kind == "captions" }
+                    .map {
+                        Video.Subtitle(
+                            label = it.label,
+                            file = it.file,
+                            default = it.default,
+                        )
+                    }
+            )
+
+            _state.postValue(State.SuccessLoading(video))
         } catch (e: Exception) {
             _state.postValue(State.FailedLoading(e))
         }
