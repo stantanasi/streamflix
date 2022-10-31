89d4f11430e2d11ed4753eabec0d0a342a4d4ecd
Lory-Stan TANASI <stantanasi@gmail.com>
2022-10-31 09:59:08 +0100

    feat(movie.model): add movies's genres property

--- app/src/main/java/com/tanasi/sflix/models/Show.kt
+++ app/src/main/java/com/tanasi/sflix/models/Show.kt
@@ -36,6 +36,7 @@ class Movie(
     val poster: String? = null,
     val banner: String? = null,
 
+    val genres: List<Genre> = listOf(),
     val cast: List<People> = listOf(),
     val recommendations: List<Show> = listOf(),
 ) : Show(), SflixAdapter.Item, Cloneable {
