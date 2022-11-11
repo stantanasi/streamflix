4bfae866fdcc31fb7564f8cad55792ed503a677b
Lory-Stan TANASI <stantanasi@gmail.com>
2022-11-11 01:39:11 +0100

    feat: create Provider interface

---
+++ app/src/main/java/com/tanasi/sflix/providers/Provider.kt
@@ -0,0 +1,29 @@
+package com.tanasi.sflix.providers
+
+import com.tanasi.sflix.fragments.player.PlayerFragment
+import com.tanasi.sflix.models.*
+
+interface Provider {
+
+    suspend fun getHome(): List<Category>
+
+    suspend fun search(query: String): List<Show>
+
+    suspend fun getMovies(): List<Movie>
+
+    suspend fun getTvShows(): List<TvShow>
+
+
+    suspend fun getMovie(id: String): Movie
+
+
+    suspend fun getTvShow(id: String): TvShow
+
+    suspend fun getSeasonEpisodes(seasonId: String): List<Episode>
+
+
+    suspend fun getPeople(id: String): People
+
+
+    suspend fun getVideo(id: String, videoType: PlayerFragment.VideoType): Video
+}
