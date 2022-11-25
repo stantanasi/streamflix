d8599886eb0da4f0f6f3988a183619e3e7508307
Lory-Stan TANASI <stantanasi@gmail.com>
2022-11-25 19:37:05 +0100

    feat: create launcher activity

---
+++ app/src/main/java/com/tanasi/sflix/activities/LauncherActivity.kt
@@ -0,0 +1,17 @@
+package com.tanasi.sflix.activities
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.tanasi.sflix.databinding.ActivityLauncherBinding
+
+class LauncherActivity : FragmentActivity() {
+
+    private var _binding: ActivityLauncherBinding? = null
+    private val binding: ActivityLauncherBinding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityLauncherBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
