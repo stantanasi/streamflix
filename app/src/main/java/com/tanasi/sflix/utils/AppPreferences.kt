a577bf12c621b57b001fc2e900a42e3438593221
Lory-Stan TANASI <stantanasi@gmail.com>
2022-11-26 23:38:39 +0100

    feat: create app preferences class

---
+++ app/src/main/java/com/tanasi/sflix/utils/AppPreferences.kt
@@ -0,0 +1,87 @@
+package com.tanasi.sflix.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.tanasi.sflix.BuildConfig
+
+object AppPreferences {
+
+    private lateinit var prefs: SharedPreferences
+
+    fun setup(context: Context) {
+        prefs = context.getSharedPreferences(
+            "${BuildConfig.APPLICATION_ID}.preferences",
+            Context.MODE_PRIVATE,
+        )
+    }
+
+
+    private enum class Key {
+        ;
+
+        fun getBoolean(): Boolean? = when {
+            prefs.contains(name) -> prefs.getBoolean(name, false)
+            else -> null
+        }
+
+        fun getFloat(): Float? = when {
+            prefs.contains(name) -> prefs.getFloat(name, 0F)
+            else -> null
+        }
+
+        fun getInt(): Int? = when {
+            prefs.contains(name) -> prefs.getInt(name, 0)
+            else -> null
+        }
+
+        fun getLong(): Long? = when {
+            prefs.contains(name) -> prefs.getLong(name, 0)
+            else -> null
+        }
+
+        fun getString(): String? = when {
+            prefs.contains(name) -> prefs.getString(name, "")
+            else -> null
+        }
+
+        fun setBoolean(value: Boolean?) = value?.let {
+            with(prefs.edit()) {
+                putBoolean(name, value)
+                apply()
+            }
+        } ?: remove()
+
+        fun setFloat(value: Float?) = value?.let {
+            with(prefs.edit()) {
+                putFloat(name, value)
+                apply()
+            }
+        } ?: remove()
+
+        fun setInt(value: Int?) = value?.let {
+            with(prefs.edit()) {
+                putInt(name, value)
+                apply()
+            }
+        } ?: remove()
+
+        fun setLong(value: Long?) = value?.let {
+            with(prefs.edit()) {
+                putLong(name, value)
+                apply()
+            }
+        } ?: remove()
+
+        fun setString(value: String?) = value?.let {
+            with(prefs.edit()) {
+                putString(name, value)
+                apply()
+            }
+        } ?: remove()
+
+        fun remove() = with(prefs.edit()) {
+            remove(name)
+            apply()
+        }
+    }
+}
