e87ab7b68ef30a0a1eec3ae76ab2fb5cee3ee857
Lory-Stan TANASI <stantanasi@gmail.com>
2023-03-22 20:38:39 +0100

    feat(genre-viewholder): implement displaying genre item

--- app/src/main/java/com/tanasi/sflix/adapters/viewholders/GenreViewHolder.kt
+++ app/src/main/java/com/tanasi/sflix/adapters/viewholders/GenreViewHolder.kt
@@ -1,7 +1,11 @@
 package com.tanasi.sflix.adapters.viewholders
 
+import android.graphics.drawable.GradientDrawable
+import android.view.animation.AnimationUtils
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
+import com.tanasi.sflix.R
+import com.tanasi.sflix.databinding.ItemGenreGridBinding
 import com.tanasi.sflix.models.Genre
 
 
@@ -18,6 +22,28 @@ class GenreViewHolder(
         this.genre = genre
 
         when (_binding) {
+            is ItemGenreGridBinding -> displayGridItem(_binding)
         }
+    }
+
+
+    private fun displayGridItem(binding: ItemGenreGridBinding) {
+        binding.root.apply {
+            val colors = context.resources.getIntArray(R.array.genres)
+            (background as? GradientDrawable)?.setColor(colors[bindingAdapterPosition % colors.size])
+
+            setOnClickListener {
+            }
+            setOnFocusChangeListener { _, hasFocus ->
+                val animation = when {
+                    hasFocus -> AnimationUtils.loadAnimation(context, R.anim.zoom_in)
+                    else -> AnimationUtils.loadAnimation(context, R.anim.zoom_out)
+                }
+                binding.root.startAnimation(animation)
+                animation.fillAfter = true
+            }
+        }
+
+        binding.tvGenreName.text = genre.name
     }
 }
