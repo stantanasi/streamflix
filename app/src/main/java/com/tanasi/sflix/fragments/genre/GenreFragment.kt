bb69dc700a132cc82d65e58f386d01e5f172c4f3
Lory-Stan TANASI <stantanasi@gmail.com>
2023-03-22 20:47:50 +0100

    feat(genre): retrieve genre's movies/TV shows

--- app/src/main/java/com/tanasi/sflix/fragments/genre/GenreFragment.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/genre/GenreFragment.kt
@@ -5,15 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.tanasi.sflix.databinding.FragmentGenreBinding
+import com.tanasi.sflix.utils.viewModelsFactory
 
 class GenreFragment : Fragment() {
 
     private var _binding: FragmentGenreBinding? = null
     private val binding get() = _binding!!
 
-    private val viewModel by viewModels<GenreViewModel>()
+    private val args by navArgs<GenreFragmentArgs>()
+    private val viewModel by viewModelsFactory { GenreViewModel(args.id) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
--- app/src/main/java/com/tanasi/sflix/fragments/genre/GenreViewModel.kt
+++ app/src/main/java/com/tanasi/sflix/fragments/genre/GenreViewModel.kt
@@ -3,9 +3,13 @@ package com.tanasi.sflix.fragments.genre
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.tanasi.sflix.models.Genre
+import com.tanasi.sflix.utils.AppPreferences
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class GenreViewModel : ViewModel() {
+class GenreViewModel(id: String) : ViewModel() {
 
     private val _state = MutableLiveData<State>(State.Loading)
     val state: LiveData<State> = _state
@@ -14,5 +18,22 @@ class GenreViewModel : ViewModel() {
         object Loading : State()
         data class SuccessLoading(val genre: Genre) : State()
         data class FailedLoading(val error: Exception) : State()
+    }
+
+    init {
+        getGenre(id)
+    }
+
+
+    private fun getGenre(id: String) = viewModelScope.launch(Dispatchers.IO) {
+        _state.postValue(State.Loading)
+
+        try {
+            val genre = AppPreferences.currentProvider.getGenre(id)
+
+            _state.postValue(State.SuccessLoading(genre))
+        } catch (e: Exception) {
+            _state.postValue(State.FailedLoading(e))
+        }
     }
 }
