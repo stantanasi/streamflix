9ce45898fbb8ecf7eeea5c6057e9b6e0655a64d4
Lory-Stan TANASI <stantanasi@gmail.com>
2023-04-02 18:57:37 +0200

    feat(launcher): show update dialog on new update available

--- app/src/main/java/com/tanasi/sflix/activities/launcher/LauncherActivity.kt
+++ app/src/main/java/com/tanasi/sflix/activities/launcher/LauncherActivity.kt
@@ -1,10 +1,12 @@
 package com.tanasi.sflix.activities.launcher
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.fragment.app.FragmentActivity
 import com.tanasi.sflix.R
 import com.tanasi.sflix.databinding.ActivityLauncherBinding
+import com.tanasi.sflix.ui.UpdateDialog
 import com.tanasi.sflix.utils.AppPreferences
 
 class LauncherActivity : FragmentActivity() {
@@ -14,6 +16,8 @@ class LauncherActivity : FragmentActivity() {
 
     private val viewModel by viewModels<LauncherViewModel>()
 
+    private lateinit var updateDialog: UpdateDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme_SFlix)
         super.onCreate(savedInstanceState)
@@ -21,5 +25,29 @@ class LauncherActivity : FragmentActivity() {
         setContentView(binding.root)
 
         AppPreferences.setup(this)
+
+        viewModel.state.observe(this) { state ->
+            when (state) {
+                LauncherViewModel.State.CheckingUpdate -> {}
+                is LauncherViewModel.State.SuccessCheckingUpdate -> {
+                    val asset = state.release?.assets
+                        ?.find { it.contentType == "application/vnd.android.package-archive" }
+                    if (asset != null) {
+                        updateDialog = UpdateDialog(this).also {
+                            it.release = state.release
+                            it.show()
+                        }
+                    }
+                }
+
+                is LauncherViewModel.State.FailedUpdate -> {
+                    Toast.makeText(
+                        this,
+                        state.error.message ?: "",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
     }
 }
