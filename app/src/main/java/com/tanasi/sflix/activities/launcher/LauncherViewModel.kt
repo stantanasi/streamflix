7b3e31e25f831813eaa32182b54fc100ac94b453
Lory-Stan TANASI <stantanasi@gmail.com>
2023-04-02 18:33:03 +0200

    feat(launcher): check if update available

--- app/src/main/java/com/tanasi/sflix/activities/launcher/LauncherViewModel.kt
+++ app/src/main/java/com/tanasi/sflix/activities/launcher/LauncherViewModel.kt
@@ -3,7 +3,11 @@ package com.tanasi.sflix.activities.launcher
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.tanasi.sflix.utils.GitHub
+import com.tanasi.sflix.utils.InAppUpdater
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.io.File
 
 class LauncherViewModel : ViewModel() {
@@ -21,5 +25,22 @@ class LauncherViewModel : ViewModel() {
         object InstallingUpdate : State()
 
         data class FailedUpdate(val error: Exception) : State()
+    }
+
+    init {
+        checkUpdate()
+    }
+
+
+    private fun checkUpdate() = viewModelScope.launch(Dispatchers.IO) {
+        _state.postValue(State.CheckingUpdate)
+
+        try {
+            val release = InAppUpdater.getReleaseUpdate()
+
+            _state.postValue(State.SuccessCheckingUpdate(release))
+        } catch (e: Exception) {
+            _state.postValue(State.FailedUpdate(e))
+        }
     }
 }
