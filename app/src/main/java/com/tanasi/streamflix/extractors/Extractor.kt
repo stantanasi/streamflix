94fb9be316c8ade830f5d30e5072c4c70bcce17f
Lory-Stan TANASI <stantanasi@gmail.com>
2023-09-27 09:59:48 +0200

    feat(extractor): implement extract sources from link

--- app/src/main/java/com/tanasi/streamflix/extractors/Extractor.kt
+++ app/src/main/java/com/tanasi/streamflix/extractors/Extractor.kt
@@ -8,4 +8,26 @@ abstract class Extractor {
     abstract val mainUrl: String
 
     abstract suspend fun extract(link: String): Video
+
+
+    companion object {
+        private val extractors = listOf(
+            Rabbitstream(),
+            Rabbitstream.Megacloud(),
+            Streamhub(),
+        )
+
+        suspend fun extract(link: String): Video {
+            val urlRegex = Regex("^(https?://)?(www\\.)?")
+            val compareUrl = link.lowercase().replace(urlRegex, "")
+            for (extractor in extractors) {
+                if (compareUrl.startsWith(extractor.mainUrl.replace(urlRegex, "")) ||
+                    compareUrl.startsWith(extractor.mainUrl.replace(Regex("^(https?://)?(www\\.)?(.*?)(\\.[a-z]+)"), "$3"))) {
+                    return extractor.extract(link)
+                }
+            }
+
+            throw Exception("No extractors found")
+        }
+    }
 }
