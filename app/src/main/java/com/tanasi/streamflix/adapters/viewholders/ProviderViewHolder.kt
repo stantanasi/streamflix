1fc4600856356007a474a1f5905c5894901d6da7
Lory-Stan TANASI <stantanasi@gmail.com>
2023-11-27 19:42:26 +0100

    fix(providers): reload app on provider change

--- app/src/main/java/com/tanasi/streamflix/adapters/viewholders/ProviderViewHolder.kt
+++ app/src/main/java/com/tanasi/streamflix/adapters/viewholders/ProviderViewHolder.kt
@@ -1,13 +1,16 @@
 package com.tanasi.streamflix.adapters.viewholders
 
+import android.content.Intent
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 import com.bumptech.glide.Glide
+import com.tanasi.streamflix.activities.main.MainActivity
 import com.tanasi.streamflix.databinding.ItemProviderBinding
 import com.tanasi.streamflix.fragments.providers.ProvidersFragmentDirections
 import com.tanasi.streamflix.models.Provider
 import com.tanasi.streamflix.utils.UserPreferences
+import com.tanasi.streamflix.utils.toActivity
 
 class ProviderViewHolder(
     private val _binding: ViewBinding
@@ -31,7 +34,10 @@ class ProviderViewHolder(
         binding.root.apply {
             setOnClickListener {
                 UserPreferences.currentProvider = provider.provider
-                findNavController().navigate(ProvidersFragmentDirections.actionProvidersToHome())
+                context.toActivity()?.apply {
+                    finish()
+                    startActivity(Intent(this, MainActivity::class.java))
+                }
             }
         }
 
