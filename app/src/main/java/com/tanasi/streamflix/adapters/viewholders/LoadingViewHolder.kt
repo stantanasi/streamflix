0379bea11363680020d6ec51414b507c001c8201
Lory-Stan TANASI <stantanasi@gmail.com>
2023-11-27 15:07:00 +0100

    feat: create Loading ViewHolder

---
+++ app/src/main/java/com/tanasi/streamflix/adapters/viewholders/LoadingViewHolder.kt
@@ -0,0 +1,10 @@
+package com.tanasi.streamflix.adapters.viewholders
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+
+class LoadingViewHolder(
+    private val _binding: ViewBinding
+) : RecyclerView.ViewHolder(
+    _binding.root
+)
