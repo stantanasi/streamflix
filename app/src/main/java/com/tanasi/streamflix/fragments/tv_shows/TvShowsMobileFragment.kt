0c02978a6fabd02280df2b1f31d1466039635b58
Lory-Stan TANASI <stantanasi@gmail.com>
2024-02-29 20:01:45 +0100

    feat(mobile): create TV shows fragment

---
+++ app/src/main/java/com/tanasi/streamflix/fragments/tv_shows/TvShowsMobileFragment.kt
@@ -0,0 +1,27 @@
+package com.tanasi.streamflix.fragments.tv_shows
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tanasi.streamflix.databinding.FragmentTvShowsMobileBinding
+
+class TvShowsMobileFragment : Fragment() {
+
+    private var _binding: FragmentTvShowsMobileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTvShowsMobileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
