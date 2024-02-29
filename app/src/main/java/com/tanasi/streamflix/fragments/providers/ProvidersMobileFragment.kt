df25113abd31386a3b1cddc5257dab0e3927eea1
Lory-Stan TANASI <stantanasi@gmail.com>
2024-02-29 19:58:55 +0100

    feat(mobile): create providers fragment

---
+++ app/src/main/java/com/tanasi/streamflix/fragments/providers/ProvidersMobileFragment.kt
@@ -0,0 +1,27 @@
+package com.tanasi.streamflix.fragments.providers
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tanasi.streamflix.databinding.FragmentProvidersMobileBinding
+
+class ProvidersMobileFragment : Fragment() {
+
+    private var _binding: FragmentProvidersMobileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProvidersMobileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
