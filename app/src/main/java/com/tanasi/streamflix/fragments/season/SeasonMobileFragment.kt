452947a8afb7e3ef6be67b90ec7115b66c23cfea
Lory-Stan TANASI <stantanasi@gmail.com>
2024-02-29 20:03:20 +0100

    feat(mobile): create season fragment

---
+++ app/src/main/java/com/tanasi/streamflix/fragments/season/SeasonMobileFragment.kt
@@ -0,0 +1,27 @@
+package com.tanasi.streamflix.fragments.season
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tanasi.streamflix.databinding.FragmentSeasonMobileBinding
+
+class SeasonMobileFragment : Fragment() {
+
+    private var _binding: FragmentSeasonMobileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSeasonMobileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
