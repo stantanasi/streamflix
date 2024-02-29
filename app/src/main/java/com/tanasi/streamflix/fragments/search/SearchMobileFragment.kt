a8b57f0d4d2a3a4e4df906589de9045361c0f07d
Lory-Stan TANASI <stantanasi@gmail.com>
2024-02-29 20:00:47 +0100

    feat(mobile): create search fragment

---
+++ app/src/main/java/com/tanasi/streamflix/fragments/search/SearchMobileFragment.kt
@@ -0,0 +1,27 @@
+package com.tanasi.streamflix.fragments.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tanasi.streamflix.databinding.FragmentSearchMobileBinding
+
+class SearchMobileFragment : Fragment() {
+
+    private var _binding: FragmentSearchMobileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchMobileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
