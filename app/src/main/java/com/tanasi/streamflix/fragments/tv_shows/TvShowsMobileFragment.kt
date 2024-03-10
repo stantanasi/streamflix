cf73450d5967fe5727a9f50158c2486f34874c54
Lory-Stan TANASI <stantanasi@gmail.com>
2024-03-10 12:05:31 +0100

    feat(tvshow-mobile): display TV shows page on mobile

--- app/src/main/java/com/tanasi/streamflix/fragments/tv_shows/TvShowsMobileFragment.kt
+++ app/src/main/java/com/tanasi/streamflix/fragments/tv_shows/TvShowsMobileFragment.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.tanasi.streamflix.adapters.AppAdapter
 import com.tanasi.streamflix.databinding.FragmentTvShowsMobileBinding
+import com.tanasi.streamflix.models.TvShow
+import com.tanasi.streamflix.ui.SpacingItemDecoration
+import com.tanasi.streamflix.utils.dp
 
 class TvShowsMobileFragment : Fragment() {
 
     private var _binding: FragmentTvShowsMobileBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel by viewModels<TvShowsViewModel>()
+
+    private val appAdapter = AppAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,5 +33,68 @@ class TvShowsMobileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initializeTvShows()
+
+        viewModel.state.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                TvShowsViewModel.State.Loading -> binding.isLoading.apply {
+                    root.visibility = View.VISIBLE
+                    pbIsLoading.visibility = View.VISIBLE
+                    gIsLoadingRetry.visibility = View.GONE
+                }
+                TvShowsViewModel.State.LoadingMore -> appAdapter.isLoading = true
+                is TvShowsViewModel.State.SuccessLoading -> {
+                    displayTvShows(state.tvShows, state.hasMore)
+                    appAdapter.isLoading = false
+                    binding.isLoading.root.visibility = View.GONE
+                }
+                is TvShowsViewModel.State.FailedLoading -> {
+                    Toast.makeText(
+                        requireContext(),
+                        state.error.message ?: "",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    if (appAdapter.isLoading) {
+                        appAdapter.isLoading = false
+                    } else {
+                        binding.isLoading.apply {
+                            pbIsLoading.visibility = View.GONE
+                            gIsLoadingRetry.visibility = View.VISIBLE
+                            btnIsLoadingRetry.setOnClickListener {
+                                viewModel.getTvShows()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+    private fun initializeTvShows() {
+        binding.rvTvShows.apply {
+            adapter = appAdapter
+            addItemDecoration(
+                SpacingItemDecoration(10.dp(requireContext()))
+            )
+        }
+    }
+
+    private fun displayTvShows(tvShows: List<TvShow>, hasMore: Boolean) {
+        appAdapter.submitList(tvShows.onEach {
+            it.itemType = AppAdapter.Type.TV_SHOW_GRID_MOBILE_ITEM
+        })
+
+        if (hasMore) {
+            appAdapter.setOnLoadMoreListener { viewModel.loadMoreTvShows() }
+        } else {
+            appAdapter.setOnLoadMoreListener(null)
+        }
     }
 }
