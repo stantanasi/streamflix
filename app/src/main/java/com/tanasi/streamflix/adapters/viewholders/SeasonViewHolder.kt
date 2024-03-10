e2a3d954f5f154d0f7c13cb383222eb1d3000b57
Lory-Stan TANASI <stantanasi@gmail.com>
2024-03-10 13:10:40 +0100

    feat(season-viewholder): implement displaying season item on mobile

--- app/src/main/java/com/tanasi/streamflix/adapters/viewholders/SeasonViewHolder.kt
+++ app/src/main/java/com/tanasi/streamflix/adapters/viewholders/SeasonViewHolder.kt
@@ -7,7 +7,9 @@ import androidx.viewbinding.ViewBinding
 import com.bumptech.glide.Glide
 import com.tanasi.streamflix.R
 import com.tanasi.streamflix.databinding.ItemSeasonBinding
+import com.tanasi.streamflix.databinding.ItemSeasonMobileBinding
 import com.tanasi.streamflix.fragments.tv_show.TvShowFragmentDirections
+import com.tanasi.streamflix.fragments.tv_show.TvShowMobileFragmentDirections
 import com.tanasi.streamflix.models.Season
 
 
@@ -24,10 +26,39 @@ class SeasonViewHolder(
         this.season = season
 
         when (_binding) {
+            is ItemSeasonMobileBinding -> displayMobileItem(_binding)
             is ItemSeasonBinding -> displayItem(_binding)
         }
     }
 
+
+    private fun displayMobileItem(binding: ItemSeasonMobileBinding) {
+        binding.root.apply {
+            setOnClickListener {
+                findNavController().navigate(
+                    TvShowMobileFragmentDirections.actionTvShowToSeason(
+                        tvShowId = season.tvShow?.id ?: "",
+                        tvShowTitle = season.tvShow?.title ?: "",
+                        tvShowPoster = season.tvShow?.poster,
+                        tvShowBanner = season.tvShow?.banner,
+                        seasonId = season.id,
+                        seasonNumber = season.number,
+                        seasonTitle = season.title,
+                    )
+                )
+            }
+        }
+
+        binding.ivSeasonPoster.apply {
+            clipToOutline = true
+            Glide.with(context)
+                .load(season.poster)
+                .centerCrop()
+                .into(this)
+        }
+
+        binding.tvSeasonTitle.text = season.title
+    }
 
     private fun displayItem(binding: ItemSeasonBinding) {
         binding.root.apply {
