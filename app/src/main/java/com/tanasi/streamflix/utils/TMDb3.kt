799f4601384980f26b2ec5095ed436ef60ab4096
Lory-Stan TANASI <stantanasi@gmail.com>
2024-03-26 18:04:48 +0100

    feat: create TMDb API utils

---
+++ app/src/main/java/com/tanasi/streamflix/utils/TMDb3.kt
@@ -0,0 +1,10 @@
+package com.tanasi.streamflix.utils
+
+import com.tanasi.streamflix.BuildConfig
+
+object TMDb3 {
+
+    private const val URL = "https://api.themoviedb.org/3/"
+    private const val API_KEY = BuildConfig.TMDB_API_KEY
+
+}
