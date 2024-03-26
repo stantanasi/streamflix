393e0df97797eaa22e7605995f73c1c38d4d679b
Lory-Stan TANASI <stantanasi@gmail.com>
2024-03-26 18:33:58 +0100

    feat(tmdb): create enum for movies genres

--- app/src/main/java/com/tanasi/streamflix/utils/TMDb3.kt
+++ app/src/main/java/com/tanasi/streamflix/utils/TMDb3.kt
@@ -59,7 +59,32 @@ object TMDb3 {
     data class Genre(
         val id: Int,
         val name: String,
-    )
+    ) {
+
+        enum class Movie(val id: Int) {
+            ACTION(28),
+            ADVENTURE(12),
+            ANIMATION(16),
+            COMEDY(35),
+            CRIME(80),
+            DOCUMENTARY(99),
+            DRAMA(18),
+            FAMILY(10751),
+            FANTASY(14),
+            HISTORY(36),
+            HORROR(27),
+            MUSIC(10402),
+            MYSTERY(9648),
+            ROMANCE(10749),
+            SCIENCE_FICTION(878),
+            TV_MOVIE(10770),
+            THRILLER(53),
+            WAR(10752),
+            WESTERN(37);
+
+            override fun toString() = id.toString()
+        }
+    }
 
     data class Movie(
         @SerializedName("poster_path") val posterPath: String?,
