9fcebbe8198ca1177d0183cbd9b017eb7e38ce34
Lory-Stan TANASI <stantanasi@gmail.com>
2024-03-26 18:35:21 +0100

    feat(tmdb): create enum for TV genres

--- app/src/main/java/com/tanasi/streamflix/utils/TMDb3.kt
+++ app/src/main/java/com/tanasi/streamflix/utils/TMDb3.kt
@@ -84,6 +84,27 @@ object TMDb3 {
 
             override fun toString() = id.toString()
         }
+
+        enum class Tv(val id: Int) {
+            ACTION_ADVENTURE(10759),
+            ANIMATION(16),
+            COMEDY(35),
+            CRIME(80),
+            DOCUMENTARY(99),
+            DRAMA(18),
+            FAMILY(10751),
+            KIDS(10762),
+            MYSTERY(9648),
+            NEWS(10763),
+            REALITY(10764),
+            SCIENCE_FICTION_FANTASY(10765),
+            SOAP(10766),
+            TALK(10767),
+            WAR_POLITICS(10768),
+            WESTERN(37);
+
+            override fun toString() = id.toString()
+        }
     }
 
     data class Movie(
