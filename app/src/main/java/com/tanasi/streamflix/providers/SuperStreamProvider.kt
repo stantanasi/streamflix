0ed659a0c8538140771b1cb1938d7507bd7987ef
Lory-Stan TANASI <stantanasi@gmail.com>
2024-03-26 21:23:38 +0100

    feat(superstream): create SuperStream responses models

--- app/src/main/java/com/tanasi/streamflix/providers/SuperStreamProvider.kt
+++ app/src/main/java/com/tanasi/streamflix/providers/SuperStreamProvider.kt
@@ -1,6 +1,7 @@
 package com.tanasi.streamflix.providers
 
 import android.util.Base64
+import com.google.gson.annotations.SerializedName
 import com.tanasi.streamflix.adapters.AppAdapter
 import com.tanasi.streamflix.models.Category
 import com.tanasi.streamflix.models.Episode
@@ -125,6 +126,314 @@ object SuperStreamProvider : Provider {
                     .build()
 
                 return retrofit.create(SuperStreamApiService::class.java)
+            }
+        }
+
+
+        data class Response<T>(
+            @SerializedName("code") val code: Int,
+            @SerializedName("msg") val msg: String,
+            @SerializedName("data") val data: T,
+        )
+
+        data class HomeResponse(
+            @SerializedName("code") val code: Int? = null,
+            @SerializedName("type") val type: String? = null,
+            @SerializedName("name") val name: String? = null,
+            @SerializedName("box_type") val boxType: Int? = null,
+            @SerializedName("list") val list: List<Show> = listOf(),
+            @SerializedName("cache") val cache: Boolean? = null,
+            @SerializedName("cacheKey") val cacheKey: String? = null,
+        )
+
+        data class Show(
+            @SerializedName("id") val id: Int,
+            @SerializedName("box_type") val boxType: Int,
+            @SerializedName("title") val title: String? = null,
+            @SerializedName("actors") val actors: String? = null,
+            @SerializedName("description") val description: String? = null,
+            @SerializedName("poster_min") val posterMin: String? = null,
+            @SerializedName("poster_org") val posterOrg: String? = null,
+            @SerializedName("poster") val poster: String? = null,
+            @SerializedName("banner_mini") val bannerMini: String? = null,
+            @SerializedName("cats") val cats: String? = null,
+//            @SerializedName("content_rating") val contentRating: Int? = null,
+            @SerializedName("runtime") val runtime: Int? = null,
+            @SerializedName("year") val year: Int? = null,
+//            @SerializedName("collect") val collect: Int? = null,
+            @SerializedName("view") val view: Int? = null,
+            @SerializedName("download") val download: Int? = null,
+            @SerializedName("imdb_rating") val imdbRating: String? = null,
+//            @SerializedName("is_collect") val isCollect: Int? = null,
+            @SerializedName("3d") val threeD: Int? = null,
+            @SerializedName("audio_lang") val audioLang: String? = null,
+            @SerializedName("quality_tag") val qualityTag: String? = null,
+            @SerializedName("lang") val lang: String? = null,
+            @SerializedName("trailer_url_arr") val trailerUrlArr: List<Trailer> = listOf(),
+            @SerializedName("trailer_url") val trailerUrl: String? = null,
+
+            @SerializedName("poster_2") val poster2: String? = null,
+            @SerializedName("season_episode") val seasonEpisode: String? = null,
+        )
+
+        data class Trailer(
+            @SerializedName("url") val url: String? = null,
+            @SerializedName("img") val img: String? = null,
+        )
+
+        data class MovieDetails(
+            @SerializedName("id") val id: Int,
+            @SerializedName("mb_id") val mbId: Int? = null,
+            @SerializedName("title") val title: String? = null,
+            @SerializedName("vip_only") val vipOnly: Int? = null,
+            @SerializedName("display") val display: Int? = null,
+            @SerializedName("state") val state: Int? = null,
+            @SerializedName("source_file") val sourceFile: Int? = null,
+            @SerializedName("code_file") val codeFile: Int? = null,
+            @SerializedName("director") val director: String? = null,
+            @SerializedName("writer") val writer: String? = null,
+            @SerializedName("actors") val actors: String? = null,
+            @SerializedName("runtime") val runtime: Int? = null,
+            @SerializedName("poster") val poster: String? = null,
+            @SerializedName("poster_imdb") val posterImdb: Int? = null,
+            @SerializedName("description") val description: String? = null,
+            @SerializedName("cats") val cats: String? = null,
+            @SerializedName("year") val year: Int? = null,
+            @SerializedName("imdb_id") val imdbId: String? = null,
+            @SerializedName("imdb_rating") val imdbRating: String? = null,
+            @SerializedName("trailer") val trailer: String? = null,
+            @SerializedName("recommend") val recommend: ArrayList<Recommend> = arrayListOf(),
+            @SerializedName("add_time") val addTime: Int? = null,
+//            @SerializedName("collect") val collect: Int? = null,
+            @SerializedName("view") val view: Int? = null,
+            @SerializedName("download") val download: Int? = null,
+            @SerializedName("released") val released: String? = null,
+            @SerializedName("released_timestamp") val releasedTimestamp: Int? = null,
+            @SerializedName("update_time") val updateTime: Int? = null,
+            @SerializedName("audio_lang") val audioLang: ArrayList<String> = arrayListOf(),
+            @SerializedName("quality_tag") val qualityTag: String? = null,
+            @SerializedName("quality_tag_new") val qualityTagNew: String? = null,
+            @SerializedName("3d") val threeD: Int? = null,
+            @SerializedName("remark") val remark: String? = null,
+            @SerializedName("pending") val pending: String? = null,
+            @SerializedName("content_rating") val contentRating: String? = null,
+            @SerializedName("tmdb_id") val tmdbId: Int? = null,
+            @SerializedName("tomato_url") val tomatoUrl: String? = null,
+            @SerializedName("tomato_meter") val tomatoMeter: Int? = null,
+            @SerializedName("tomato_meter_count") val tomatoMeterCount: Int? = null,
+            @SerializedName("tomato_meter_state") val tomatoMeterState: String? = null,
+            @SerializedName("reelgood_url") val reelgoodUrl: String? = null,
+            @SerializedName("audience_score") val audienceScore: Int? = null,
+            @SerializedName("audience_score_count") val audienceScoreCount: Int? = null,
+            @SerializedName("no_tomato_url") val noTomatoUrl: Int? = null,
+            @SerializedName("weights") val weights: Double? = null,
+            @SerializedName("1080p") val quality1080p: Int? = null,
+            @SerializedName("short_tag") val shortTag: String? = null,
+            @SerializedName("poster_min") val posterMin: String? = null,
+            @SerializedName("poster_org") val posterOrg: String? = null,
+            @SerializedName("trailer_url_arr") val trailerUrlArr: ArrayList<Trailer> = arrayListOf(),
+            @SerializedName("trailer_url") val trailerUrl: String? = null,
+            @SerializedName("quality") val quality: ArrayList<String> = arrayListOf(),
+            @SerializedName("quality_tags") val qualityTags: ArrayList<String> = arrayListOf(),
+            @SerializedName("imdb_link") val imdbLink: String? = null,
+            @SerializedName("seconds") val seconds: Int? = null,
+//            @SerializedName("is_collect") val isCollect: Int? = null,
+            @SerializedName("box_type") val boxType: Int? = null,
+            @SerializedName("language") val language: ArrayList<Language> = arrayListOf(),
+            @SerializedName("playlists") val playlists: ArrayList<Playlists> = arrayListOf()
+        )
+
+        data class Recommend(
+            @SerializedName("mid") val mid: Int? = null,
+            @SerializedName("title") val title: String? = null,
+            @SerializedName("poster_min") val posterMin: String? = null,
+            @SerializedName("poster_org") val posterOrg: String? = null,
+            @SerializedName("poster") val poster: String? = null,
+            @SerializedName("quality_tag") val qualityTag: String? = null,
+            @SerializedName("imdb_rating") val imdbRating: String? = null,
+            @SerializedName("audio_lang") val audioLang: String? = null,
+            @SerializedName("runtime") val runtime: Int? = null,
+            @SerializedName("cats") val cats: String? = null,
+            @SerializedName("year") val year: Int? = null,
+            @SerializedName("3d") val threeD: Int? = null
+
+        )
+
+        data class Playlists(
+            @SerializedName("lid") val lid: Int? = null,
+            @SerializedName("name") val name: String? = null,
+            @SerializedName("username") val username: String? = null,
+            @SerializedName("count") val count: Int? = null,
+            @SerializedName("imgArr") val imgArr: ArrayList<String> = arrayListOf(),
+            @SerializedName("imgArr_min") val imgArrMin: ArrayList<String> = arrayListOf(),
+            @SerializedName("imgArr_org") val imgArrOrg: ArrayList<String> = arrayListOf()
+        )
+
+        data class TvShow(
+            @SerializedName("id") val id: Int,
+            @SerializedName("mb_id") val mbId: Int? = null,
+            @SerializedName("title") val title: String? = null,
+            @SerializedName("display") val display: Int? = null,
+            @SerializedName("state") val state: Int? = null,
+            @SerializedName("vip_only") val vipOnly: Int? = null,
+            @SerializedName("code_file") val codeFile: Int? = null,
+            @SerializedName("director") val director: String? = null,
+            @SerializedName("writer") val writer: String? = null,
+            @SerializedName("actors") val actors: String? = null,
+            @SerializedName("add_time") val addTime: Int? = null,
+            @SerializedName("poster") val poster: String? = null,
+            @SerializedName("poster_imdb") val posterImdb: Int? = null,
+            @SerializedName("banner_mini") val bannerMini: String? = null,
+            @SerializedName("description") val description: String? = null,
+            @SerializedName("imdb_id") val imdbId: String? = null,
+            @SerializedName("cats") val cats: String? = null,
+            @SerializedName("year") val year: Int? = null,
+//            @SerializedName("collect") val collect: Int? = null,
+            @SerializedName("view") val view: Int? = null,
+            @SerializedName("download") val download: Int? = null,
+            @SerializedName("update_time") val updateTime: String? = null,
+            @SerializedName("released") val released: String? = null,
+            @SerializedName("released_timestamp") val releasedTimestamp: Int? = null,
+            @SerializedName("episode_released") val episodeReleased: String? = null,
+            @SerializedName("episode_released_timestamp") val episodeReleasedTimestamp: Int? = null,
+            @SerializedName("max_season") val maxSeason: Int? = null,
+            @SerializedName("max_episode") val maxEpisode: Int? = null,
+            @SerializedName("remark") val remark: String? = null,
+            @SerializedName("imdb_rating") val imdbRating: String? = null,
+            @SerializedName("content_rating") val contentRating: String? = null,
+            @SerializedName("tmdb_id") val tmdbId: Int? = null,
+            @SerializedName("tomato_url") val tomatoUrl: String? = null,
+            @SerializedName("tomato_meter") val tomatoMeter: Int? = null,
+            @SerializedName("tomato_meter_count") val tomatoMeterCount: Int? = null,
+            @SerializedName("tomato_meter_state") val tomatoMeterState: String? = null,
+            @SerializedName("reelgood_url") val reelgoodUrl: String? = null,
+            @SerializedName("audience_score") val audienceScore: Int? = null,
+            @SerializedName("audience_score_count") val audienceScoreCount: Int? = null,
+            @SerializedName("no_tomato_url") val noTomatoUrl: Int? = null,
+            @SerializedName("order_year") val orderYear: Int? = null,
+            @SerializedName("episodate_id") val episodateId: String? = null,
+            @SerializedName("weights_day") val weightsDay: Double? = null,
+            @SerializedName("short_tag") val shortTag: String? = null,
+            @SerializedName("poster_min") val posterMin: String? = null,
+            @SerializedName("poster_org") val posterOrg: String? = null,
+            @SerializedName("banner_mini_min") val bannerMiniMin: String? = null,
+            @SerializedName("banner_mini_org") val bannerMiniOrg: String? = null,
+            @SerializedName("trailer_url") val trailerUrl: String? = null,
+            @SerializedName("years") val years: ArrayList<Int> = arrayListOf(),
+            @SerializedName("season") val season: ArrayList<Int> = arrayListOf(),
+            @SerializedName("history") val history: ArrayList<String> = arrayListOf(),
+            @SerializedName("imdb_link") val imdbLink: String? = null,
+            @SerializedName("episode") val episode: ArrayList<Episode> = arrayListOf(),
+//            @SerializedName("is_collect") val isCollect: Int? = null,
+            @SerializedName("language") val language: ArrayList<Language> = arrayListOf(),
+            @SerializedName("box_type") val boxType: Int? = null,
+            @SerializedName("year_year") val yearYear: String? = null,
+            @SerializedName("season_episode") val seasonEpisode: String? = null
+        )
+
+        data class Language(
+            @SerializedName("title") val title: String? = null,
+            @SerializedName("lang") val lang: String? = null
+        )
+
+        data class Episode(
+            @SerializedName("id") val id: Int,
+            @SerializedName("tid") val tid: Int? = null,
+            @SerializedName("mb_id") val mbId: Int? = null,
+            @SerializedName("imdb_id") val imdbId: String? = null,
+            @SerializedName("imdb_id_status") val imdbIdStatus: Int? = null,
+            @SerializedName("srt_status") val srtStatus: Int? = null,
+            @SerializedName("season") val season: Int,
+            @SerializedName("episode") val episode: Int,
+            @SerializedName("state") val state: Int? = null,
+            @SerializedName("title") val title: String? = null,
+            @SerializedName("thumbs") val thumbs: String? = null,
+            @SerializedName("thumbs_bak") val thumbsBak: String? = null,
+            @SerializedName("thumbs_original") val thumbsOriginal: String? = null,
+            @SerializedName("poster_imdb") val posterImdb: Int? = null,
+            @SerializedName("synopsis") val synopsis: String? = null,
+            @SerializedName("runtime") val runtime: Int? = null,
+            @SerializedName("view") val view: Int? = null,
+            @SerializedName("download") val download: Int? = null,
+            @SerializedName("source_file") val sourceFile: Int? = null,
+            @SerializedName("code_file") val codeFile: Int? = null,
+            @SerializedName("add_time") val addTime: Int? = null,
+            @SerializedName("update_time") val updateTime: Int? = null,
+            @SerializedName("released") val released: String? = null,
+            @SerializedName("released_timestamp") val releasedTimestamp: Long? = null,
+            @SerializedName("audio_lang") val audioLang: String? = null,
+            @SerializedName("quality_tag") val qualityTag: String? = null,
+            @SerializedName("3d") val threeD: Int? = null,
+            @SerializedName("remark") val remark: String? = null,
+            @SerializedName("pending") val pending: String? = null,
+            @SerializedName("imdb_rating") val imdbRating: String? = null,
+            @SerializedName("display") val display: Int? = null,
+            @SerializedName("sync") val sync: Int? = null,
+            @SerializedName("tomato_meter") val tomatoMeter: Int? = null,
+            @SerializedName("tomato_meter_count") val tomatoMeterCount: Int? = null,
+            @SerializedName("tomato_audience") val tomatoAudience: Int? = null,
+            @SerializedName("tomato_audience_count") val tomatoAudienceCount: Int? = null,
+            @SerializedName("thumbs_min") val thumbsMin: String? = null,
+            @SerializedName("thumbs_org") val thumbsOrg: String? = null,
+            @SerializedName("imdb_link") val imdbLink: String? = null,
+//        @SerializedName("quality_tags") val qualityTags: List<String> = listOf(),
+//        @SerializedName("play_progress") val playProgress: PlayProgress? = PlayProgress()
+        )
+
+
+        data class LinkResponse(
+            @SerializedName("seconds") val seconds: Int? = null,
+            @SerializedName("quality") val quality: List<String> = listOf(),
+            @SerializedName("list") val list: List<Link> = listOf(),
+        ) {
+            data class Link(
+                @SerializedName("path") val path: String? = null,
+                @SerializedName("quality") val quality: String? = null,
+                @SerializedName("real_quality") val realQuality: String? = null,
+                @SerializedName("format") val format: String? = null,
+                @SerializedName("size") val size: String? = null,
+                @SerializedName("size_bytes") val sizeBytes: Long? = null,
+                @SerializedName("count") val count: Int? = null,
+                @SerializedName("dateline") val dateline: Long? = null,
+                @SerializedName("fid") val fid: Int? = null,
+                @SerializedName("mmfid") val mmfid: Int? = null,
+                @SerializedName("h265") val h265: Int? = null,
+                @SerializedName("hdr") val hdr: Int? = null,
+                @SerializedName("filename") val filename: String? = null,
+                @SerializedName("original") val original: Int? = null,
+                @SerializedName("colorbit") val colorbit: Int? = null,
+                @SerializedName("success") val success: Int? = null,
+                @SerializedName("timeout") val timeout: Int? = null,
+                @SerializedName("vip_link") val vipLink: Int? = null,
+                @SerializedName("fps") val fps: Int? = null,
+                @SerializedName("bitstream") val bitstream: String? = null,
+                @SerializedName("width") val width: Int? = null,
+                @SerializedName("height") val height: Int? = null,
+            )
+        }
+
+        data class PrivateSubtitleData(
+            @SerializedName("select") val select: ArrayList<String> = arrayListOf(),
+            @SerializedName("list") val list: ArrayList<SubtitleList> = arrayListOf()
+        ) {
+            data class SubtitleList(
+                @SerializedName("language") val language: String? = null,
+                @SerializedName("subtitles") val subtitles: ArrayList<Subtitles> = arrayListOf()
+            ) {
+                data class Subtitles(
+                    @SerializedName("sid") val sid: Int? = null,
+                    @SerializedName("mid") val mid: String? = null,
+                    @SerializedName("file_path") val filePath: String? = null,
+                    @SerializedName("lang") val lang: String? = null,
+                    @SerializedName("language") val language: String? = null,
+                    @SerializedName("delay") val delay: Int? = null,
+                    @SerializedName("point") val point: String? = null,
+                    @SerializedName("order") val order: Int? = null,
+                    @SerializedName("admin_order") val adminOrder: Int? = null,
+                    @SerializedName("myselect") val myselect: Int? = null,
+                    @SerializedName("add_time") val addTime: Long? = null,
+                    @SerializedName("count") val count: Int? = null
+                )
             }
         }
     }
