f4fac442d79af7abec0a9c25de29713465280e3d
Lory-Stan TANASI <stantanasi@gmail.com>
2024-04-12 14:25:25 +0200

    feat(extensions): implement combine for 6 flows

--- app/src/main/java/com/tanasi/streamflix/utils/Extensions.kt
+++ app/src/main/java/com/tanasi/streamflix/utils/Extensions.kt
@@ -22,6 +22,8 @@ import androidx.navigation.fragment.NavHostFragment
 import com.tanasi.streamflix.R
 import com.tanasi.streamflix.activities.main.MainTvActivity
 import com.tanasi.streamflix.activities.main.MainMobileActivity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
 import kotlinx.parcelize.Parcelize
 import java.text.SimpleDateFormat
 import java.util.Calendar
@@ -217,4 +219,31 @@ fun <K, V> Map<K, V?>.filterNotNullValues() = filterValues { it != null } as Map
 fun <T> List<T>.safeSubList(fromIndex: Int, toIndex: Int): List<T> {
     if (fromIndex > toIndex) return emptyList()
     return subList(max(min(fromIndex.coerceAtLeast(0), size), 0), max(min(toIndex.coerceAtMost(size), size), 0))
+}
+
+inline fun <T1, T2, T3, T4, T5, T6, R> combine(
+    flow: Flow<T1>,
+    flow2: Flow<T2>,
+    flow3: Flow<T3>,
+    flow4: Flow<T4>,
+    flow5: Flow<T5>,
+    flow6: Flow<T6>,
+    crossinline transform: suspend (T1, T2, T3, T4, T5, T6) -> R
+): Flow<R> = combine(
+    flow,
+    flow2,
+    flow3,
+    flow4,
+    flow5,
+    flow6,
+) { args: Array<*> ->
+    @Suppress("UNCHECKED_CAST")
+    transform(
+        args[0] as T1,
+        args[1] as T2,
+        args[2] as T3,
+        args[3] as T4,
+        args[4] as T5,
+        args[5] as T6,
+    )
 }
