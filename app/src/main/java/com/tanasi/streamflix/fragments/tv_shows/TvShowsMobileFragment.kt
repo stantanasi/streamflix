15224a1ba87e86d8a0d44b3ae4df3a04901952d8
Lory-Stan TANASI <stantanasi@gmail.com>
2024-04-13 23:22:35 +0200

    feat(tv-shows): use Flow to retrieve TV shows from database

--- app/src/main/java/com/tanasi/streamflix/fragments/tv_shows/TvShowsMobileFragment.kt
+++ app/src/main/java/com/tanasi/streamflix/fragments/tv_shows/TvShowsMobileFragment.kt
@@ -6,19 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.tanasi.streamflix.adapters.AppAdapter
+import com.tanasi.streamflix.database.AppDatabase
 import com.tanasi.streamflix.databinding.FragmentTvShowsMobileBinding
 import com.tanasi.streamflix.models.TvShow
 import com.tanasi.streamflix.ui.SpacingItemDecoration
 import com.tanasi.streamflix.utils.dp
+import com.tanasi.streamflix.utils.viewModelsFactory
+import kotlinx.coroutines.launch
 
 class TvShowsMobileFragment : Fragment() {
 
     private var _binding: FragmentTvShowsMobileBinding? = null
     private val binding get() = _binding!!
 
-    private val viewModel by viewModels<TvShowsViewModel>()
+    private val database by lazy { AppDatabase.getInstance(requireContext()) }
+    private val viewModel by viewModelsFactory { TvShowsViewModel(database) }
 
     private val appAdapter = AppAdapter()
 
@@ -36,33 +42,35 @@ class TvShowsMobileFragment : Fragment() {
 
         initializeTvShows()
 
-        viewModel.state.observe(viewLifecycleOwner) { state ->
-            when (state) {
-                TvShowsViewModel.State.Loading -> binding.isLoading.apply {
-                    root.visibility = View.VISIBLE
-                    pbIsLoading.visibility = View.VISIBLE
-                    gIsLoadingRetry.visibility = View.GONE
-                }
-                TvShowsViewModel.State.LoadingMore -> appAdapter.isLoading = true
-                is TvShowsViewModel.State.SuccessLoading -> {
-                    displayTvShows(state.tvShows, state.hasMore)
-                    appAdapter.isLoading = false
-                    binding.isLoading.root.visibility = View.GONE
-                }
-                is TvShowsViewModel.State.FailedLoading -> {
-                    Toast.makeText(
-                        requireContext(),
-                        state.error.message ?: "",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    if (appAdapter.isLoading) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.state.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED).collect { state ->
+                when (state) {
+                    TvShowsViewModel.State.Loading -> binding.isLoading.apply {
+                        root.visibility = View.VISIBLE
+                        pbIsLoading.visibility = View.VISIBLE
+                        gIsLoadingRetry.visibility = View.GONE
+                    }
+                    TvShowsViewModel.State.LoadingMore -> appAdapter.isLoading = true
+                    is TvShowsViewModel.State.SuccessLoading -> {
+                        displayTvShows(state.tvShows, state.hasMore)
                         appAdapter.isLoading = false
-                    } else {
-                        binding.isLoading.apply {
-                            pbIsLoading.visibility = View.GONE
-                            gIsLoadingRetry.visibility = View.VISIBLE
-                            btnIsLoadingRetry.setOnClickListener {
-                                viewModel.getTvShows()
+                        binding.isLoading.root.visibility = View.GONE
+                    }
+                    is TvShowsViewModel.State.FailedLoading -> {
+                        Toast.makeText(
+                            requireContext(),
+                            state.error.message ?: "",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        if (appAdapter.isLoading) {
+                            appAdapter.isLoading = false
+                        } else {
+                            binding.isLoading.apply {
+                                pbIsLoading.visibility = View.GONE
+                                gIsLoadingRetry.visibility = View.VISIBLE
+                                btnIsLoadingRetry.setOnClickListener {
+                                    viewModel.getTvShows()
+                                }
                             }
                         }
                     }
--- app/src/main/java/com/tanasi/streamflix/fragments/tv_shows/TvShowsViewModel.kt
+++ app/src/main/java/com/tanasi/streamflix/fragments/tv_shows/TvShowsViewModel.kt
@@ -1,19 +1,52 @@
 package com.tanasi.streamflix.fragments.tv_shows
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.tanasi.streamflix.database.AppDatabase
 import com.tanasi.streamflix.models.TvShow
 import com.tanasi.streamflix.utils.UserPreferences
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.transformLatest
 import kotlinx.coroutines.launch
 
-class TvShowsViewModel : ViewModel() {
+class TvShowsViewModel(database: AppDatabase) : ViewModel() {
 
-    private val _state = MutableLiveData<State>(State.Loading)
-    val state: LiveData<State> = _state
+    private val _state = MutableStateFlow<State>(State.Loading)
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val state: Flow<State> = combine(
+        _state,
+        _state.transformLatest { state ->
+            when (state) {
+                is State.SuccessLoading -> {
+                    database.tvShowDao().getByIds(state.tvShows.map { it.id })
+                        .collect { emit(it) }
+                }
+                else -> emit(emptyList<TvShow>())
+            }
+        },
+    ) { state, tvShowsDb ->
+        when (state) {
+            is State.SuccessLoading -> {
+                State.SuccessLoading(
+                    tvShows = state.tvShows.map { tvShow ->
+                        tvShowsDb.find { it.id == tvShow.id }
+                            ?.takeIf { !tvShow.isSame(it) }
+                            ?.let { tvShow.copy().merge(it) }
+                            ?: tvShow
+                    },
+                    hasMore = state.hasMore
+                )
+
+            }
+            else -> state
+        }
+    }
 
     private var page = 1
 
@@ -30,31 +63,31 @@ class TvShowsViewModel : ViewModel() {
 
 
     fun getTvShows() = viewModelScope.launch(Dispatchers.IO) {
-        _state.postValue(State.Loading)
+        _state.emit(State.Loading)
 
         try {
             val tvShows = UserPreferences.currentProvider!!.getTvShows()
 
             page = 1
 
-            _state.postValue(State.SuccessLoading(tvShows, true))
+            _state.emit(State.SuccessLoading(tvShows, true))
         } catch (e: Exception) {
             Log.e("TvShowsViewModel", "getTvShows: ", e)
-            _state.postValue(State.FailedLoading(e))
+            _state.emit(State.FailedLoading(e))
         }
     }
 
     fun loadMoreTvShows() = viewModelScope.launch(Dispatchers.IO) {
-        val currentState = state.value
+        val currentState = _state.first()
         if (currentState is State.SuccessLoading) {
-            _state.postValue(State.LoadingMore)
+            _state.emit(State.LoadingMore)
 
             try {
                 val tvShows = UserPreferences.currentProvider!!.getTvShows(page + 1)
 
                 page += 1
 
-                _state.postValue(
+                _state.emit(
                     State.SuccessLoading(
                         tvShows = currentState.tvShows + tvShows,
                         hasMore = tvShows.isNotEmpty(),
@@ -62,7 +95,7 @@ class TvShowsViewModel : ViewModel() {
                 )
             } catch (e: Exception) {
                 Log.e("TvShowsViewModel", "loadMoreTvShows: ", e)
-                _state.postValue(State.FailedLoading(e))
+                _state.emit(State.FailedLoading(e))
             }
         }
     }
