5c3ec78124777296d014037d1a6e9c12ba010f61
Lory-Stan TANASI <stantanasi@gmail.com>
2024-10-15 15:08:39 +0200

    feat: create AniWorld provider

---
+++ app/src/main/java/com/tanasi/streamflix/providers/AniWorldProvider.kt
@@ -0,0 +1,87 @@
+package com.tanasi.streamflix.providers
+
+import com.tanasi.streamflix.adapters.AppAdapter
+import com.tanasi.streamflix.models.Category
+import com.tanasi.streamflix.models.Episode
+import com.tanasi.streamflix.models.Genre
+import com.tanasi.streamflix.models.Movie
+import com.tanasi.streamflix.models.People
+import com.tanasi.streamflix.models.TvShow
+import com.tanasi.streamflix.models.Video
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+
+object AniWorldProvider : Provider {
+
+    override val name = "AniWorld"
+    override val logo = ""
+    override val language = "de"
+    private const val URL = "https://aniworld.to/"
+
+    private val service = Service.build()
+
+    override suspend fun getHome(): List<Category> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun search(query: String, page: Int): List<AppAdapter.Item> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getMovies(page: Int): List<Movie> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTvShows(page: Int): List<TvShow> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getMovie(id: String): Movie {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTvShow(id: String): TvShow {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getEpisodesBySeason(seasonId: String): List<Episode> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getGenre(id: String, page: Int): Genre {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getPeople(id: String, page: Int): People {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getServers(id: String, videoType: Video.Type): List<Video.Server> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getVideo(server: Video.Server): Video {
+        TODO("Not yet implemented")
+    }
+
+
+    private interface Service {
+
+        companion object {
+            fun build(): Service {
+                val client = OkHttpClient.Builder()
+                    .readTimeout(30, TimeUnit.SECONDS)
+                    .connectTimeout(30, TimeUnit.SECONDS)
+                    .build()
+
+                val retrofit = Retrofit.Builder()
+                    .baseUrl(URL)
+                    .client(client)
+                    .build()
+
+                return retrofit.create(Service::class.java)
+            }
+        }
+    }
+}
