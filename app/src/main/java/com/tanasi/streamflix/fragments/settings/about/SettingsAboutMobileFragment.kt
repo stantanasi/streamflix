1119790bbc9ced122c3b7f4c064d74cc450da2fc
Lory-Stan TANASI <stantanasi@gmail.com>
2024-11-04 16:26:14 +0100

    feat(settingsabout-mobile): display version name

--- app/src/main/java/com/tanasi/streamflix/fragments/settings/about/SettingsAboutMobileFragment.kt
+++ app/src/main/java/com/tanasi/streamflix/fragments/settings/about/SettingsAboutMobileFragment.kt
@@ -1,12 +1,22 @@
 package com.tanasi.streamflix.fragments.settings.about
 
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.tanasi.streamflix.BuildConfig
 import com.tanasi.streamflix.R
 
 class SettingsAboutMobileFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_about_mobile, rootKey)
+
+        displaySettingsAbout()
+    }
+
+    private fun displaySettingsAbout() {
+        findPreference<Preference>("p_settings_about_version")?.apply {
+            summary = getString(R.string.settings_about_version_name, BuildConfig.VERSION_NAME)
+        }
     }
 }
