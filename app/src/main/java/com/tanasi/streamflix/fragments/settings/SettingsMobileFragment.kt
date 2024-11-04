f3951b5bcb5fada94e9017359ccfc3700dd484bc
Lory-Stan TANASI <stantanasi@gmail.com>
2024-11-04 16:16:05 +0100

    feat(settings-mobile): navigate to settings about

--- app/src/main/java/com/tanasi/streamflix/fragments/settings/SettingsMobileFragment.kt
+++ app/src/main/java/com/tanasi/streamflix/fragments/settings/SettingsMobileFragment.kt
@@ -1,6 +1,8 @@
 package com.tanasi.streamflix.fragments.settings
 
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.tanasi.streamflix.R
 
@@ -8,5 +10,18 @@ class SettingsMobileFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_mobile, rootKey)
+
+        displaySettings()
+    }
+
+    private fun displaySettings() {
+        findPreference<Preference>("p_settings_about")?.apply {
+            setOnPreferenceClickListener {
+                findNavController().navigate(
+                    SettingsMobileFragmentDirections.actionSettingsToSettingsAbout()
+                )
+                true
+            }
+        }
     }
 }
