2fd275a0202bc4beeb207c3e6f90f5effbe6ad87
PaduU29 <zinelalexandru29@gmail.com>
2025-08-05 12:10:33 +0200

    StreamWishExtractor.kt fix, use webview to get redirected url

--- app/src/main/java/com/tanasi/streamflix/StreamFlixApp.kt
+++ app/src/main/java/com/tanasi/streamflix/StreamFlixApp.kt
@@ -6,8 +6,13 @@ import com.tanasi.streamflix.providers.AniWorldProvider
 import com.tanasi.streamflix.providers.SerienStreamProvider
 
 class StreamFlixApp: Application() {
+    companion object {
+        lateinit var instance: StreamFlixApp
+            private set
+    }
     override fun onCreate() {
         super.onCreate()
+        instance = this
         SerienStreamProvider.initialize(this)
         AniWorldProvider.initialize(this)
     }
